d59ab7104ef1920ed2a4352939672b71504832dc
Rahul-Sagore <rahul.sagore@gmail.com>
2013-08-16 03:04:36 +0530

    Added Footer file with css updation

---
+++ carousel/footer.php
@@ -0,0 +1,25 @@
+
+      <!-- FOOTER -->
+<footer>
+<div id="footer" class="foot" >
+  <div class="wrapper">
+  		<div>
+		  <table width="100%" height="75">
+		    <tr>
+			<td>IIPS ,DAVV<br />
+			    Takshashila Campus, Khandwa Road<br />
+				Indore, India, 452001.
+			</td>
+            <td  valign="bottom"><a href="#"> About This Site</a>
+			 <footer>
+        	&copy; 2013 Developement Center, IIPS &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p> </footer></td>
+			<td>
+			  Stay Connected : <br />
+			  <a href="#">Feedback</a>
+			 </td>
+			 </tr>
+			</table> 
+			<!--<p class="pull-right"><a href="#">Back to top</a></p> -->
+</div>
+</div>
+</footer>
