a0f90ed192598ef4c32f39b69c5d3bf753ed9c1c
Rahul-Sagore <rahul.sagore@gmail.com>
2013-08-16 03:01:44 +0530

    Added newsEvent tab file

---
+++ carousel/newsEvents.php
@@ -0,0 +1,45 @@
+ <div style="width:710px; margin-left:95px;"> 
+	
+	<div class="panel panel-primary" style="height:200px">
+  		<div class="panel-heading">
+    		<h3 class="panel-title">News & Events</h3>
+  		</div>
+  		<div class="panel-body">
+       		<ul class="nav nav-tabs" id="myTab">
+         		<li class="active"><a href="#news">News</a></li>
+         		<li><a href="#events">Events</a></li>
+        	</ul>
+       		<div class="tab-content">
+       		  <div class="tab-pane active" id="news">
+				<ul class="media-list">
+  					<li class="media">
+   						 <a class="pull-left" href="#">
+     						 <img class="media-object" src="..." alt="...">
+   						 </a>
+   					 	<div class="media-body">
+     					 <h4 class="media-heading">Best in Computer Science</h4>
+      							IIPS is awarded as the Best Institute for Computer Science
+    					</div>
+  					</li>
+				</ul>
+
+			</div>
+       		<div class="tab-pane" id="events">
+				<ul class="media-list">
+  					<li class="media">
+   						 <a class="pull-left" href="#">
+     						 <img class="media-object" src="..." alt="...">
+   						 </a>
+   					 	<div class="media-body">
+     					 <h4 class="media-heading">Expressions</h4>
+      							Expression Coming Soon
+    					</div>
+  					</li>
+				</ul>
+			</div>
+      	</div>
+	</div>
+</div>
+</div>
+<br>
+<br>
