ab7c8c1752aa4391c479794479a8079f8537976a
Rahul-Sagore <rahul.sagore@gmail.com>
2013-08-16 19:03:17 +0530

    Added syenergy page for events menu

---
+++ carousel/synergy.php
@@ -0,0 +1,68 @@
+ <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+        <title>About IIPS</title>
+
+       <!-- Include Styles -->
+        <link rel="stylesheet" href="css/vtab-style.css" />		
+        <!--[if IE 7]><style type="text/css">#v-nav>ul>li.current{border-right:1px solid #fff!important}#v-nav>div.tab-content{z-index:-1!important;left:0}</style><![endif]-->
+        <!--[if IE 8]><style type="text/css">#v-nav>ul>li.current{border-right:1px solid #fff!important}#v-nav>div.tab-content{z-index:-1!important;left:0}</style><![endif]-->
+		<?php
+		 include('cssLinks.php');
+		?>
+		<style type="text/css">
+			.iips_logo{
+				float:right;
+			}
+		</style>
+    </head>
+    <body>
+		<?php
+			include('header.php');
+		?>
+<div style="width:960px; margin-left:100px">
+<b>Synergy</b> <br /><br />
+<pre>Synergy is the annual sports event conducted at 'IIPS' since last few years. It provides the students with a podium to showcase their team spirit hone their leadership qualities and inculcates sportsman spirit in them. It not only helps to crack the ice between the students of different courses but also helps the new students to overcome their inhibition and gel along with the rest of the college. 'Synergy' provides an opportunity to break the monotonous academic schedule and test other life skills.The three day event shall include cricket, football, badminton, volleyball, basketball, athletics, table tennis; at the end of which everyone emerges a winner, some with medals some without. As a tradition synergy is organized by the students of third year with the support from the rest of the college in event management which shall further come to their avail in organizing the other major events in IIPS, viz, 'X!pressions'(organized mainly by the students of fourth year) and 'Samvardhan'(organized by the students of fifth year).The tears shed, the sweat dropped,the giggles,the occasional frowns, all make the event memorable and instills in the participants a new zeal to excel and reach the zenith.
+
+			
+
+
+<b>Proposed Date</b>
+
+    Last week of January or first week of February
+
+<b>Events</b>
+	<ul type="disc">
+    <li>Cricket (for boys and girls)</li>
+    <li>Football (only for boys)</li>
+    <li>Basketball (for boys and girls)</li>
+    <li>Volleyball (for boys and girls)</li>
+    <li>Badminton (for boys and girls)</li>
+    <li>Table-Tennis (for boys and girls)</li>
+    <li>Athletics (for boys and girls)</li>
+	</ul>
+
+<b>Places for conducting the event</b>
+    <ul type="disc">
+    <li>U.T.D. sports ground</li>
+    <li>Basketball court (in front of auditorium)</li>
+    <li>Gymnasium</li>
+    <li>IIPS Hostel ground</li>
+    </ul>	
+</pre>
+</div>
+
+<?php
+			include('jsLinks.php');
+		?>
+		<!-- Include Scripts for vertical tabs-->
+		<script type="text/javascript" src="js/jQuery-hashchange.js"></script>       
+        <script type="text/javascript" src="js/vtab-script.js"></script>
+		
+<!-- container 12 ended -->
+</body>
+</html>
