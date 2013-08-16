16b182cbbe02774969448b4eb738bac4ede35cb2
Rahul-Sagore <rahul.sagore@gmail.com>
2013-08-16 21:32:33 +0530

    Added file for MBA-MS Placement

---
+++ carousel/mbaMsPlacement.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+        <title>Events</title>
+
+       <!-- Include Styles -->
+        <link rel="stylesheet" href="css/vtab-style.css" />		
+        <!--[if IE 7]><style type="text/css">#v-nav>ul>li.current{border-right:1px solid #fff!important}#v-nav>div.tab-content{z-index:-1!important;left:0}</style><![endif]-->
+        <!--[if IE 8]><style type="text/css">#v-nav>ul>li.current{border-right:1px solid #fff!important}#v-nav>div.tab-content{z-index:-1!important;left:0}</style><![endif]-->
+		<?php
+		 include('cssLinks.php');
+		?>
+    </head>
+    <body>
+		<?php
+			include('header.php');
+		?>
+		<section id="wrapper" class="wrapper">
+
+			<h1> Events </h1>
+            <div id="v-nav">
+
+                <ul>
+                    <li tab="tab1" class="first current" style="z-index:0">Placement Officer</li>
+                    <li tab="tab2">Batch Profile</li>
+                    <li tab="tab3">Last Year Placement</li>
+					<li tab="tab4">Corporate Recruiter</li>
+					<li tab="tab5" class="last">Recruitment Procedure</li>
+                </ul>
+
+                <div class="tab-content">
+					<div class="panel panel-success">
+  						<div class="panel-heading">
+							<h3 class="panel-title">MBA - Placement Officer's Message</h3>
+  						</div>
+ 				        <div class="panel-body">
+    					     
+<pre>
+I feel honoured to invite you to this world of professionalism and commitment.
+
+The world progresses ahead with new technologies mushrooming almost everyday. In such a scenario someone should take responsibility to train the young generation in the right direction, so that they are at par with the latest technologies. I take pride in announcing that IIPS is one such institution, which has not only taken the above responsibility but is also handling it proficiently. Our latest batch of young professionals is working hard to be an essential part of this dynamic software industry. The intensive training and the encouraging learning environment of our institute has grilled in them not only an extensive subject knowledge but also a sense of commitment, diligence and the value of hard work.
+
+I am confident that after having an inside glimpse of IIPS through this brochure you would definitely be interested in testing our students by coming for the On-Campus recruitment. Furthermore I would like to express my gratitude towards those companies which have shown confidence in our students and have come in the past years for the On-Campus placements. They have become an eternal part of the lIPS family and have always received much more than their expectations from us.
+-- 
+
+Please feel free to contact us at any point of time for any information or queries.
+
+Imroz Khan
+Mob.No.-919425351233
+Off -91-0731-2468507
+Mail id - imroz@iips.edu.in</pre>
+
+  				        </div>
+                    </div>
+                </div>
+	<div class="tab-content" style="text-align:justify; padding:0px 20px 0px 20px; margin-top:0px; line-height:1.5;">
+				<div class="panel panel-success">
+  						<div class="panel-heading">
+							<h3 class="panel-title">Batch Profile : MBA</h3>
+  						</div>
+ 				        <div class="panel-body">
+    					     <table border="1px" bordercolor="#000000" style="background-color:#FFFFFF" width="100%" cellpadding="3" cellspacing="3">
+	<tr bgcolor="#0066FF">
+		<td>Name</td>
+		<td>Age</td>
+		<td>Specializationt</td>
+	</tr>
+	<tr>
+		<td>Aadesh Patidar</td>
+		<td>24</td>
+		<td>Marketing</td>
+	</tr>
+	<tr>
+		<td>Abhay Jaiswal</td>
+		<td>24</td>
+		<td>Finance</td>
+	</tr>
+	<tr>
+		<td>Abhigyan Dube</td>
+		<td>23</td>
+		<td>Finance</td>
+	</tr>
+	<tr>
+		<td>Abhishek Tiwari</td>
+		<td>22</td>
+		<td>Marketing</td>
+	</tr>
+	<tr>
+		<td>Aditi Golecha</td>
+		<td>24</td>
+		<td>Marketing</td>
+	</tr>
+	<tr>
+		<td>Aditi Joshi</td>
+		<td>21</td>
+		<td>Finance</td>
+	</tr>
+	<tr>
+		<td>Aditi Panwar</td>
+		<td>23</td>
+		<td>Finance</td>
+	</tr>
+	<tr>
+		<td>Aditya Prakash Mishra</td>
+		<td>22</td>
+		<td>HR</td>
+	</tr>
+	<tr>
+		<td>Aditya Tiwari</td>
+		<td>23</td>
+		<td>Marketing</td>
+	</tr>
+</table>
+
+
+  				        </div>
+                    </div>
+
+                </div>
+
+                <div class="tab-content">
+                   <div class="panel panel-success">
+  						<div class="panel-heading">
+    						<h3 class="panel-title">Earlier Placements 2011-2012 </h3>
+  						</div>
+  						<div class="panel-body">
+  
+<table border="1px">
+<tr bgcolor="#0066CC" ><td>Students</td> <td>Company </td></tr>
+<tr><td>Arpit Verma</td> <td>Shoppers Stop</td></tr>
+<tr><td>Atul Goyal</td> <td>ICICI Securities</td></tr>
+<tr><td>Abhigyan Dubey</td>Delloite<td></td></tr>
+<tr><td>Abhishek Tiwari</td> <td>ICICI Bank</td></tr>
+<tr><td>Aditi Joshi</td> <td>Central Bank Of India</td></tr>
+<tr><td>Aditi Panwar</td> <td>Central Bank Of India</td></tr>
+<tr><td>Aditya Tiwari</td> <td> 	TCS</td></tr>
+<tr><td>Aditya Yadav </td> <td>Shoppers Stop</td></tr>
+<tr><td>Adwait Verma</td> <td> 	ICICI Securities</td></tr>
+<tr><td>Akhil Nigudkar</td> <td> 	ICICI Securities</td></tr>
+<tr><td>Shruti Badnagarkar </td> <td>ICICI Bank </td></tr>
+<tr><td>Amit Dhabhai</td> <td> 	ICICI Securities</td></tr>
+<tr><td>Amitesh Vyas</td> <td></td>Greenback Forex</tr>
+<tr><td>Ankit Singh Sisodia</td> <td> 	Spectrum</td></tr>
+</table>
+					    </div>
+				     </div>
+                  
+                </div>
+				
+				<div class="tab-content">
+					<div class="panel panel-success">
+  						<div class="panel-heading">
+    						<h3 class="panel-title">They Believe in IIPS</h3>
+  						</div>
+  						<div class="panel-body">
+						
+						</div>
+				     </div>
+				</div>
+				<div class="tab-content">
+					<div class="panel panel-success">
+  						<div class="panel-heading">
+    						<h3 class="panel-title">Recruitment Procedure</h3>
+  						</div>
+  						<div class="panel-body">
+							 <pre>The Campus Recruitment Programme 2009-2010 of the International Institute of Professional Studies offers 98 students in business management for your esteemed organization. It comprises of 40 students of MBA(MS)(5Yr.) and 58 students of MBA(MS)(2Yr) programme.
+
+The entire gamut of placement related activities is coordinated by the placement committee. It comprises of faculty members and student representatives. The prime aim of the placement committee is to develop information bank on organizations and help students identify the area of interest and plan their career accordingly. It also acts as a recruitment coordinating intermediary in between the students and corporate organization.
+
+You are requested to send all the relevant details of the placement requirement of your organization in the format provided in brochure. The detailed curriculum-vita of the students if required shall be sent to your organization.
+
+Pre-Placement Talk
+
+The pre-placement talks delivered by organization shall provide an excellent opportunity to the students and yourself to assess each other. The student shall gain an insight into the organization, career prospects, job-profile, likely remuneration offered and above all, the values and visions of your organization.
+
+You shall be provided with audio-visual aids such as OHP, computers with presentation software etc. to make your presentation effective. The entire session shall be conducted along informal lines and students shall raise their queries about company details.
+
+For further details, please contact:
+
+Mr. Imroz Khan
+Placement officer-MBA(MS)
+International Institute of Professional Studies
+Devi Ahilya Vishwavidyalaya
+Takshashila Campus
+Khandwa Road, Indore-452001
+TeleFax: +91 (731) 2468507</pre>
+						</div>
+				     </div>
+				</div>
+				
+				
+				
+            </div>
+         
+
+		</section>
+		<?php
+			include('jsLinks.php');
+		?>
+		<!-- Include Scripts for vertical tabs-->
+		<script type="text/javascript" src="js/jQuery-hashchange.js"></script>       
+        <script type="text/javascript" src="js/vtab-script.js"></script>
