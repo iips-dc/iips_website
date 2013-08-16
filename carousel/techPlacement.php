9d055d778112b288c7c913dc40b203422fbde99d
Rahul-Sagore <rahul.sagore@gmail.com>
2013-08-16 21:31:53 +0530

    Added file for MCA/M.tech Placement

---
+++ carousel/techPlacement.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+        <title>Events</title>
+
+       <!-- Include Styles -->
+        <link rel="stylesheet" href="css/vtab-style.css" />		
+        <!--[if IE 7]><style type="text/css">#v-nav>ul>li.current{border-right:1px solid #fff!important}#v-nav>div.tab-content{z-index:-1!important;left:0}</style><![endif]-->
+        <!--[if IE 8]><style type="text/css">#v-nav>ul>li.current{border-right:1px solid #fff!important}#v-nav>div.tab-content{z-index:-1!important;left:0}</style><![endif]-->
+		<?php
+		 include('cssLinks.php');
+		?>
+    </head>
+    <body>
+		<?php
+			include('header.php');
+		?>
+		<section id="wrapper" class="wrapper">
+
+			<h1> Events </h1>
+            <div id="v-nav">
+
+                <ul>
+                    <li tab="tab1" class="first current" style="z-index:0">Placement Officer</li>
+                    <li tab="tab2">Batch Profile</li>
+                    <li tab="tab3">Last Year Placement</li>
+					<li tab="tab4" class="last">Corporate Recruiter</li>
+                </ul>
+
+                <div class="tab-content">
+					<div class="panel panel-success">
+  						<div class="panel-heading">
+							<h3 class="panel-title">MCA - Placement Officer's Message</h3>
+  						</div>
+ 				        <div class="panel-body">
+    					     
+<pre>
+I feel honoured to invite you to this world of professionalism and commitment.
+
+The world progresses ahead with new technologies mushrooming almost everyday. In such a scenario someone should take responsibility to train the young generation in the right direction, so that they are at par with the latest technologies. I take pride in announcing that IIPS is one such institution, which has not only taken the above responsibility but is also handling it proficiently. Our latest batch of young professionals is working hard to be an essential part of this dynamic software industry. The intensive training and the encouraging learning environment of our institute has grilled in them not only an extensive subject knowledge but also a sense of commitment, diligence and the value of hard work.
+
+I am confident that after having an inside glimpse of IIPS through this brochure you would definitely be interested in testing our students by coming for the On-Campus recruitment. Furthermore I would like to express my gratitude towards those companies which have shown confidence in our students and have come in the past years for the On-Campus placements. They have become an eternal part of the lIPS family and have always received much more than their expectations from us.
+-- 
+
+Please feel free to contact us at any point of time for any information or queries.
+
+Imroz Khan
+Mob.No.-919425351233
+Off -91-0731-2468507
+Mail id - imroz@iips.edu.in</pre>
+
+  				        </div>
+                    </div>
+                </div>
+
+                <div class="tab-content" style="text-align:justify; padding:0px 20px 0px 20px; margin-top:0px; line-height:1.5;">
+				<div class="panel panel-success">
+  						<div class="panel-heading">
+							<h3 class="panel-title">Batch Profile : MCA/M.TECH</h3>
+  						</div>
+ 				        <div class="panel-body">
+    					     <table border="1px" bordercolor="#000000" style="background-color:#FFFFFF" width="100%" cellpadding="3" cellspacing="3">
+	<tr bgcolor="#0066FF">
+		<td>Name</td>
+		<td>Age</td>
+		<td>Area of Interest</td>
+	</tr>
+	<tr>
+		<td>Aayush Shrimal</td>
+		<td>22</td>
+		<td>C,,Database</td>
+	</tr>
+	<tr>
+		<td>Abhishek Parihar</td>
+		<td>22</td>
+		<td>C,RDBMS-Oracle</td>
+	</tr>
+	<tr>
+		<td>Akash Gupta</td>
+		<td>22</td>
+		<td>C , HTML 5, Ethical Hacking, Internet Marketing</td>
+	</tr>
+	<tr>
+		<td>Amber jain</td>
+		<td>22</td>
+		<td>Computational Intelligence, Cryptography, Unix, C/Python/Octave/MATLAB/HTML/CSS</td>
+	</tr>
+	<tr>
+		<td>Ambesh Kukreja</td>
+		<td>20</td>
+		<td>c, c++ and database management</td>
+	</tr>
+	<tr>
+		<td>Anchal Nema</td>
+		<td>23</td>
+		<td>C,JAVA,Operating System,RDBMS</td>
+	</tr>
+	<tr>
+		<td>Anirudh Rawat</td>
+		<td>22</td>
+		<td>Software Engineering and System Analysis and Design
+</td>
+	</tr>
+	<tr>
+		<td>Arun mehra</td>
+		<td>24</td>
+		<td>JAVA , DBMS,PL/SQL</td>
+	</tr>
+	<tr>
+		<td>Arundyuti Mishra</td>
+		<td>22</td>
+		<td>C,DBMS</td>
+	</tr>
+</table>
+
+
+  				        </div>
+                    </div>
+
+                </div>
+
+                <div class="tab-content">
+                   <div class="panel panel-success">
+  						<div class="panel-heading">
+    						<h3 class="panel-title">Panel title</h3>
+  						</div>
+  						<div class="panel-body">
+  
+<table border="1px">
+<tr bgcolor="#0066CC" ><td>Students(IC-2K6 & IT-2K7)</td> <td>Placement </td></tr>
+<tr><td>Honey Verma </td> <td>ATOS  </td></tr>
+<tr><td>Bhoopendra Singh Kushwah </td>Fiserv.  <td></td></tr>
+<tr><td>Arpit Saxena </td> <td> Infosys</td></tr>
+<tr><td>Mayank Raghuwanshi </td> <td> Infosys</td></tr>
+<tr><td>Amit Singh Hora</td> <td>Infosys</td></tr>
+<tr><td>Chetali Kekre </td> <td>Infosys</td></tr>
+<tr><td>Avnish Shukla </td> <td>Infosys</td></tr>
+<tr><td>Harshal Rane </td> <td>Infosys</td></tr>
+<tr><td>Ashwani Tomar </td> <td>Infosys</td></tr>
+<tr><td>Shruti Badnagarkar </td> <td>Accenture/Infosys </td></tr>
+<tr><td>Sumit Kumar Singh </td> <td>Infosys</td></tr>
+<tr><td>Ruchika Rao </td> <td></td>Infosys</tr>
+<tr><td>Charu Panwar</td> <td>Infosys</td></tr>
+</table>
+					    </div>
+				     </div>
+                  
+                </div>
+				
+				<div class="tab-content">
+					<div class="panel">
+  						<div class="panel-heading">
+    						<h3 class="panel-title">They Believe in IIPS</h3>
+  						</div>
+  						<div class="panel-body">
+						
+						</div>
+				     </div>
+				</div>
+				
+				
+				
+                </div>
+         
+
+		</section>
+		<?php
+			include('jsLinks.php');
+		?>
+		<!-- Include Scripts for vertical tabs-->
+		<script type="text/javascript" src="js/jQuery-hashchange.js"></script>       
+        <script type="text/javascript" src="js/vtab-script.js"></script>
+		
+<!-- container 12 ended -->
+</body>
+</html>
+
