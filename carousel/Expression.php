71ce25c7bcdc4426fe949dc652b51de5ef3e3dff
Rahul-Sagore <rahul.sagore@gmail.com>
2013-08-16 19:03:59 +0530

    Added expression.php file for event's menu

---
+++ carousel/Expression.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+        <title>Events</title>
+
+       <!-- Include Styles -->
+        <link rel="stylesheet" href="css/vtab-style.css" />		
+        <!--[if IE 7]><style type="text/css">#v-nav>ul>li.current{border-right:1px solid #fff!important}#v-nav>div.tab-content{z-index:-1!important;left:0}</style><![endif]-->
+        <!--[if IE 8]><style type="text/css">#v-nav>ul>li.current{border-right:1px solid #fff!important}#v-nav>div.tab-content{z-index:-1!important;left:0}</style><![endif]-->
+		<?php
+		 include('cssLinks.php');
+		?>
+    </head>
+    <body>
+		<?php
+			include('header.php');
+		?>
+		<section id="wrapper" class="wrapper">
+
+			<h1> Events </h1>
+            <div id="v-nav">
+
+                <ul>
+                    <li tab="tab1" class="first current" style="z-index:0">The Event</li>
+                    <li tab="tab2">Winners</li>
+                    <li tab="tab3">Last Year Team</li>
+					<li tab="tab4">Expression Album</li>
+					<li tab="tab5">Expression Videos</li>
+                    <li tab="tab6">Sponsors</li>
+					<li tab="tab7" class="last">Register Your Team</li>
+                </ul>
+
+                <div class="tab-content">
+					<img class="iips_logo" src="images/xptest.jpg" align="middle">
+                    <pre>Xpressions is an annual event to incorporate the finer aspects of team building, co-ordination, Time management, effectiveness and efficiency. It aims to stimulate minds in pursuit of knowledge by exposing them to recent advancements in technological and managerial world. The two-day event is filled with fun and frolic in almost all the fields of professional world.We 'The IIPS Family' make it an occasion in which every nook and corner of the Management and Technical field are covered. It is an effort to fuse the practical aspects with a lot of knowledge generation. We can say that this Xpress!ons is the best way to expose the young minds to the world of business and technology so that they get prepared in the best way possible.
+
+Various aspects of practical skills are being emphasized like decision making, presentation, creative, technical, people oriented, organizational, team-oriented skills etc. It's an arena where every element of this world are required to be showcased which will ensure that participants, organizers and the whole IIPS will be in a win-win situation.
+The event contain games from different fields. </pre>
+                </div>
+
+                <div class="tab-content" style="text-align:justify; padding:0px 20px 0px 20px; margin-top:0px; line-height:1.5;">
+				<b>2012 Winners</b>
+              <table border="1px" >
+			  
+			    <tr><td>Runners up :</td>	<td>Ek Tarfa</td> <td></td></tr>
+<tr><td>No</td> <td> 	Student Name </td>	<td>Course </td> <td>	Batch</td></tr>
+<tr><td>1 </td> <td>	Avijit Singh Thakur </td>	<td>	MMS  </td> <td>	2008</td></tr>
+<tr><td>2</td> <td> 	Harpreet Kaur Khanuja </td>	<td>	MMS  </td> <td>	2008</td></tr>
+<tr><td>3</td> <td> 	Ankur Pandey 	</td>	<td>MMS </td> <td>	2008</td></tr>
+<tr><td>4</td> <td> 	Shashank Shrivastav(SENAPATI) </td>	<td>	MMS </td> <td>	2008</td></tr>
+<tr><td>5 </td> <td>	Tanu Chadar  </td>	<td>	MMS </td> <td>	2009</td></tr>
+<tr><td>6 </td> <td>	Priyesh Wanker </td>	<td>	MMS </td> <td>	2009</td></tr>
+<tr><td>7</td> <td> 	Shubee Jaiswal </td>	<td>	MMS </td> <td>	2010</td></tr>
+<tr><td>8 </td> <td>	Rishab Singh Chohan </td>	<td>	MMS </td> <td>	2010</td></tr>
+<tr><td>9 </td> <td>	Tanvi Bajaj </td>	<td>	MMS </td> <td>	2011</td></tr>
+<tr><td>10 </td> <td>	Shirali Jain </td>	<td>	MMS </td> <td>	2009</td></tr>
+<tr><td>11 </td> <td>	Pratap Singh Parmar   </td>	<td>	MBA 2yr </td> <td>	2010</td></tr>
+<tr><td>12</td> <td> 	Priya Kushwah </td>	<td>	MCA </td> <td>	2008</td></tr>
+<tr><td>13 </td> <td>	Bhavana Pandey </td>	<td>	MCA </td> <td>	2008</td></tr>
+<tr><td>14</td> <td> 	Snigdha Jain  </td>	<td>	MCA </td> <td>	2009</td></tr>
+<tr><td>15</td> <td> 	Sarthak Pandit </td>	<td>	MCA </td> <td>	2010</td></tr>
+<tr><td>16</td> <td> 	Paras Soni </td>	<td> 	B.COM </td> <td>	2011</td></tr></table>
+<br>
+<table border="1px">
+<tr><td>Winners : </td> <td>	Dhinka Chika</td> <td></td></tr>
+<tr><td>No</td> <td> 	Student Name </td> <td>	Course </td> <td>	Batch</td></tr>
+<tr><td>1</td> <td> 	Hitesh Kukreja </td> <td>	MMS </td> <td>	2007</td></tr>
+<tr><td>2 </td> <td>	Naynee Kukreja </td> <td>	MMS </td> <td>	2008</td></tr>
+<tr><td>3</td> <td> 	Paryul Jain 	</td> <td>MMS </td> <td>	2007</td></tr>
+<tr><td>4</td> <td> 	Ankit Gupta </td> <td>	MMS </td> <td>	2007</td></tr>
+<tr><td>5</td> <td> 	Sourabh Kanase</td> <td> 	MTech </td> <td>	2007</td></tr>
+<tr><td>6 </td> <td>	Kunal Malik </td> <td>	</td> <td>	</td></tr>
+<tr><td>7 </td> <td>	Kapil Behrani </td> <td>	</td> <td>	</td></tr>
+<tr><td>8 </td> <td>	Mahek Mangwani</td> <td> 	</td> <td>	</td></tr>
+<tr><td>9 </td> <td>	Bhavya Arora 	</td> <td>MMS  </td> <td> 	2010</td></tr>
+<tr><td>10</td> <td> 	Gourav Parmar </td> <td>	MTech </td> <td>	2010</td></tr>
+<tr><td>11</td> <td> 	Abhishek Kar 	</td> <td>BCom </td> <td>	</td></tr>
+<tr><td>12</td> <td> 	Kapil Jeswani </td> <td> </td> <td>		</td></tr>
+<tr><td>13 </td> <td>	Khushboo 	</td> <td>MMS </td> <td>	2011</td></tr>
+<tr><td>14</td> <td> 	Nikita Kak </td> <td>	MCA </td> <td>	2010</td></tr>
+<tr><td>15</td> <td> 	Nancy Nayak </td> <td>	BCom </td> <td>	2011</td></tr>
+<tr><td>16 </td> <td>	Venu Shastri </td> <td>	MCA </td> <td>	2010</td></tr>
+</table>
+
+                </div>
+
+                <div class="tab-content">
+                   <b> Teams</b><br><br>
+				   <img src="images/xptest2.jpg" align="middle">
+<table border="1px">
+<tr><td>No</td> <td> 	Team Name </td> <td>	Team Leader </td> <td>	Course </td> <td>	Year</td></tr>
+<tr><td>1</td> <td> 	HARPOONERS</td> <td> 	saurabh nagar </td> <td>	MCA </td> <td>	View Team</td></tr>
+<tr><td>2</td> <td> 	rdfz 	</td> <td>RANJAN MODI </td> <td>	MBA (MS) 2 </td> <td>	View Team</td></tr>
+<tr><td>3</td> <td> 	funtoosh</td> <td> 	PRASHANT SHINDE </td> <td>	BCOM(Hons.) </td> <td>	View Team</td></tr>
+<tr><td>4 </td> <td>	shauryas </td> <td>	NEELAM TOURANI </td> <td>	MBA (MS) 2 </td> <td>	View Team</td></tr>
+<tr><td>5 </td> <td>	Yaba Daba Doo </td> <td>	ERA Londhe </td> <td>	MCA </td> <td>	View Team</td></tr>
+<tr><td>6</td> <td> 	sin sinati babla boo </td> <td>	KURNAL BHATT </td> <td>	MBA(APR) 	</td> <td>View Team</td></tr>
+<tr><td>7 </td> <td>	TEZ DHAAR</td> <td> 	Vivek Kataria 	</td> <td>MBA(MS) 5 </td> <td>	View Team</td></tr>
+<tr><td>8 </td> <td>	zehreelay </td> <td>	AMIT DUKRE</td> <td> 	MBA(MS) 5 	</td> <td>View Team</td></tr>
+<tr><td>9 </td> <td>	Farzee </td> <td>	Sarabjeet Singh Saluja </td> <td>	MCA </td> <td>	View Team</td></tr>
+<tr><td>10</td> <td> 	Bol Bum </td> <td>	Rohit Betala 	</td> <td>BCOM(Hons.) 	</td> <td>View Team</td></tr>
+<tr><td>11</td> <td> 	Desi Katte</td> <td> 	Kirti Vardhan Banthia </td> <td>	MBA(MS) 5 </td> <td>	View Team</td></tr>
+<tr><td>12 </td> <td>	Twisters </td> <td>	ABUZAR RIYAZ HASHMI</td> <td> 	MBA(TA) 	</td> <td>View Team </td></tr>
+</table>
+
+                  
+                </div>
+				
+				<div class="tab-content">
+					<b>Photos</b><br><br>
+					<img src="images/exp/govinda.JPG">
+					 
+				</div>
+				
+				<div class="tab-content">
+					<b>Xpression's Videos</b><br><br>
+					<ul type="disc">
+					<li><a href="http://www.youtube.com/watch?feature=player_embedded&v=F12bBO_owfI"> cool dance at xpressions dus bahne</a></li>
+					</ul>
+				</div>
+				
+                <div class="tab-content">
+                    <b>Sponsors</b><br><br>
+				
+                </div>
+				<div class="tab-content">
+                    <b>Registration Form</b><br><br>
+					
+<pre><h3>Rules for Team Registration:</h3>
+
+    <h4>Team :</h4>
+        Its going to be the most fun way to imrove ur Intelligence Quotient�and the stage for this unique battle of brains has been set at IIPS�.coz what they still don�t teach u at IIT�s and IIM is to boo whistle and clap. 
+
+    <h4>16 members :</h4>
+        Hoping each �gender� should have a fair representation, u would need to have a minimum to 6 male and female members as the case maybe. At least 5 students from UG . At least 5 from PG Courses . 
+
+    <h4>Team Leader :</h4>
+        There has to be someone guiding the team. You may have not one but 2 people doing this job of which 1 has to be a girl.
+
+    <h4>Name :</h4>
+        The next step would be to come up with a suitable & appropriately sounding brand.
+
+    <h4>Colour :</h4>
+        All working according to the plan as of now to put everything in its rite place why don�t u come up with what u plan to adorn in the most soothing & eye-catching colours.
+
+    <h4>Registration</h4>
+        Registration fee for each team is Rs 2000/- (non refundable). Registrations will be done one week prior to the event.(30 Jan-4th Feb) The teams are requested to submit the above information in a sealed envelope to the Xpression�s Organizing Team to; Arpit & Atul @ D.C.
+    <h4>Latest by :</h4>
+        The allotment of the team name & colures would be held at 0100hrs on 5th Feb 2012, in the presence of all the Team Leaders. The teams are requested to submit 3 names & 3 colours in order of preference as the allotment would be held by draw of lots.
+    <h4>General Details</h4>
+     Event would be a 2 day program on 10th and 11 Feb 2012. Participation in every event from every team is compulsory. For non-participation negative marking will be done. Details of every event and schedule for 2 days will be given to the teams after registration
+        Arpit Verma 9165846115
+        Atul Goyal 9755375571
+        Development Center:0731-24668507</pre>
+
+				
+                </div>
+         
+
+		</section>
+		<?php
+			include('jsLinks.php');
+		?>
+		<!-- Include Scripts for vertical tabs-->
+		<script type="text/javascript" src="js/jQuery-hashchange.js"></script>       
+        <script type="text/javascript" src="js/vtab-script.js"></script>
+		
+<!-- container 12 ended -->
+</body>
+</html>
+
