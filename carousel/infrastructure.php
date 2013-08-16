d1e11af5985e03deed71abe5a0a17c1bb11c7193
Rahul-Sagore <rahul.sagore@gmail.com>
2013-08-16 15:30:19 +0530

    Added Infrastrucure's page for About_Us Menu

---
+++ carousel/infrastructure.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+        <title>Infrastructure</title>
+
+       <!-- Include Styles -->
+        <link rel="stylesheet" href="css/vtab-style.css" />		
+        <!--[if IE 7]><style type="text/css">#v-nav>ul>li.current{border-right:1px solid #fff!important}#v-nav>div.tab-content{z-index:-1!important;left:0}</style><![endif]-->
+        <!--[if IE 8]><style type="text/css">#v-nav>ul>li.current{border-right:1px solid #fff!important}#v-nav>div.tab-content{z-index:-1!important;left:0}</style><![endif]-->
+		<?php
+		 include('cssLinks.php');
+		?>
+</head>
+
+<body>
+	<?php
+			include('header.php');
+	?>
+	<section id="wrapper" class="wrapper">
+
+			<h1> Infrastructure </h1><br>
+            <div id="v-nav">
+
+                <ul>
+                    <li tab="tab1" class="first current" style="z-index:0">Lecture Theaters</li>
+                    <li tab="tab2">Computer Labs</li>
+                    <li tab="tab3">Libraries</li>
+					<li tab="tab4">Hostels</li>
+
+                </ul>
+
+                <div class="tab-content">
+					<b>Class rooms & Lecture Theatres</b><br>
+                    The lush green campus of IIPS includes an academic complex of classroom, seminar room, an auditorium, a well equipped library, computer center and administrative offices. the institute has two air conditioned, well equipped lecture theatres which are used for seminars and lectures. These lectures have played host to some of the most well known personalities of the corporate world. 
+ 
+                </div>
+
+                <div class="tab-content" style="text-align:justify; padding:0px 20px 0px 20px; margin-top:0px; line-height:1.5;">
+		   <b>Computer Laboratory</b><br><br>
+
+			IIPS boasts state of the art computer and electronics laboratories offering its students great opportunities            to enhance and support their theoretical knowledge with practical experience.<br><br>
+
+           <b>Main Computer Lab</b><br><br>
+
+            The main laboratory is well equipped with IBM As/400 Advanced Server Model 40S Power PC-AS 64 bit RISC Processor/96 MB RAM /2.4 GB HDD/CDROM/2.5 GB Cartridge Tape/Ethernet Adapter with software including AS-400, DB2, Compilers for COBOL , C and RPG, CICS etc. with 100 nodes including 30 IBM Thin Clients has been installed at the Computer Lab and is open to all the students of the university.<br><br><br>
+
+            <b>Internet Connectivity</b><br><br>
+
+
+            Internet connection with 512 kbps leased line with connectivity in each classroom, faculty rooms, computer labs, conference room and library. A total of 140 nodes make the overall network spread all over the IIPS.<br><br>
+
+<b>Workstations and Stand Alone Machines</b><br><br>
+
+
+Pentium based Graphic and Multimedia workstations and also numerous Pentium based stand alone machines from IBM, HCL, WIPRO and Compaq.<br><br>
+
+           <b>Local Area Networks</b><br><br>
+
+
+            Novel Netware LAN IBM P-1 Server with 20 nodes
+            Win NT 4.0 LAN on IBM Netfinity Server with 30 nodes.(Pentium 4)
+            Linux � IBM Netfinity � 30 nodes (Pentium 4) + 30 nodes (IBM Thin Clients)<br><br>
+
+            <b>Peripherals</b><br><br>
+
+
+             2 Online UPS, several multimedia kits, colour scanners, laser printers, several dot matrix and color inkjet printers.<br><br>
+
+            <b>Operating Systems</b><br><br>
+
+
+             OS/2 Warp 3.0, SCO UNIX, Novell Netware 3.1, MS-DOS 6.22, Linux, Win NT 4.0, Windows 2000,Windows Xp Professional - SP2<br><br>
+
+             <b>Lab Incharge</b><br><br>
+
+
+              Mr. Yogendra Singh Bawal is the incharge of the Computer Laboratory.
+
+          </div>
+
+                <div class="tab-content">
+                   <b>IIPS Library</b><br><br>
+
+"The Power of Knowledge..." The best, the latest, the most complete and a vast collection of Books, Journals, Magazines and subscriptions, giving students the most quiet and concentrating environment to read.
+
+IIPS Library has a collection of Books, International Journals, National Journals, Project Reports, Software and Information including the CD-ROM databases and the online databases are made available through Institute�s network. Users can access the online databases and also find out the real-time availability of library materials from their own computer terminal and Magazines. Over 1,10,000 Volumes, 140 International Journals, 250 Indian Journals, 3 libraries.
+
+<b>IIPS Digital Library</b><br>
+
+
+IIPS gives access to the state of the art digital library to its students. The library has been developed and is being maintained by the students of the institute themselves. The students also have access to three libraries:<br>
+   <ul type="disc">
+    <li>IIPS Departmental Library</li>
+    <li>The Central Library, Takshashila Campus</li>
+    <li>Student Home Library, Nalanda Campus</li>
+   </ul>	<br>
+The library system has a collection of more than one lac volumes. It represents a wide spectra of subjects with a sizeable collection on Computer Science and Management.The IIPS Library with its modern collection of knowledge resources and innovative information services fills an essential role for students, faculty, and the surrounding community in their intellectual pursuits. It is a hybrid library with the state-of-the-art technological applications. The Library holds knowledge resources predominantly related to Information Technology management and allied subjects. The Library offers a range of information services set to the highest professional standards.
+
+
+
+                  
+                </div>
+				
+				<div class="tab-content">
+					<b>Hostels</b><br>
+					 
+
+
+The students of IIPS are provided with excellent hostel facilities by the university. There are single and double seater furnished rooms having all necessary amenities. There is a community hall with television and indoor sports facilities like carom, chess etc. The Institute provides comfortable accommodation with security.
+
+The various university hostels where the IIPS students reside are:
+
+<b>Boys Hostel</b><br>
+
+Jawaharlal Lal Nehru Boys Hostel
+IMS Boys Hostel
+Rabindra Nath Tagore Boys Hostel
+
+<b>Girls Hostel</b><br>
+
+Kamla Nehru Girls Hostel
+New Girls Hostel
+C.V. Raman Girls Hostel
+
+				</div>
+				
+				
+                </div>
+
+            </div>
+
+		</section>
+		<?php
+			include('jsLinks.php');
+		?>
+		<!-- Include Scripts for vertical tabs-->
+		<script type="text/javascript" src="js/jQuery-hashchange.js"></script>       
+        <script type="text/javascript" src="js/vtab-script.js"></script>
+</body>
+</html>
