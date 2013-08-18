1ed3f11e6068c86cc3037fa9b267a446e1f4096f
Rahul-Sagore <rahul.sagore@gmail.com>
2013-08-18 19:02:27 +0530

    Added Notices and Testimonials Panel on HomePage

---
+++ carousel/notice.php
@@ -0,0 +1,44 @@
+<div id="notice" style="float:right; margin-top:-320px; width:230px; ">
+	<div class="panel panel-primary" style="height:300px;">
+  		<div class="panel-heading">
+   			 <h3 class="panel-title">Notices</h3>
+ 	    </div>
+        <div class="panel-body">
+          <ul style="margin-left:-10px;">
+			<li> NAAC : Coming Soon !</li>
+			<li> Internal Exam : Next Week</li>
+		  </ul>
+  		</div>
+    </div>
+	<div class="panel panel-primary" style=" width:230px;height:200px;">
+  		<div class="panel-heading">
+    		<h3 class="panel-title">Testimonials</h3>
+  		</div>
+  		<div class="panel-body" style="color:#000000;">
+   			 
+			 	<ul id="button_quotes" style="width:230px; float:left; margin-left:-30px; list-style-type:none;">
+        			<li>
+            			" We Can't wait to see this
+						 Website on internet "<br><br>
+            			<cite>- World</cite></pre>
+        			</li>
+        			<li>
+           				 " Finally IIPS website is working FINE !! "<br><br>
+           				 <cite>&mdash; Students</cite>
+        			</li>
+       				 <li>
+         			     " But still you are far away from
+					     Professional Website "<br><br>
+           				 <cite>&mdash; Testers</cite>
+        			</li>
+        			<li>
+          			 " We will hire those Developers !! "<br><br>
+            			<cite>&mdash; Google</cite>
+       				 </li>
+    			</ul>  
+			 
+  		</div>
+	</div>
+</div>
+
+
