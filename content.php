553946900f11af8c6ae0f9773aeb9237543430c2
Ankit-Kulkarni <ankit.kul1890@gmail.com>
2013-11-02 00:28:31 +0530

    added quicklinks.php link and removed the whole quicklink content

--- content.php
+++ content.php
@@ -1,3 +1,7 @@
+<!-- This page serves the content for Home page. It includes
+slider, notice, news_events box, Link to quicklink file
+and testimonial section -->
+
 <div class="container text-center">
 	<br>
 	<!-- 1st span12 started "slider + notices" ended -->
@@ -128,61 +132,7 @@
 
 	<div class="row">
    	   <div class="col-md-9 quicklist">
-     	 <div class="row">
-	        <div class= "col-md-2 quicklinklist">
-			    <h4> Option 1</h4>
-				<ul>
-					<li><a>Item 1</a></li>
-					<li><a>Item 2</a></li>
-					<li><a>Item 3</a></li>
-					<li><a>Item 4</a></li>
-					<li><a>Item 5</a></li>	
-				</ul>
-	   	   	</div>  
-		  	<div class= "col-md-2 quicklinklist">
-				<h4> Option 2</h4>
-				<ul>
-					<li><a href = "#">Item 1</a></li>
-					<li><a href = "#">Item 2</a></li>
-					<li><a href = "#">Item 3</a></li>
-					<li><a href = "#">Item 4</a></li>
-					<li><a href = "#">Item 5</a></li>
-				</ul>
-		 	</div>
-		
-			<div class= "col-md-2 quicklinklist">
-		   	 	<h4> Option 3</h4>
-				<ul>
-					<li><a href = "#">Item 1</a></li>
-					<li><a href = "#">Item 2</a></li>
-					<li><a href = "#">Item 3</a></li>
-					<li><a href = "#">Item 4</a></li>
-					<li><a href = "#">Item 5</a></li>
-				</ul>
-			</div>
-		
-			<div class= "col-md-2 quicklinklist">
-				<h4> Option 4</h4>
-				<ul>
-					<li><a href = "#">Item 1</a></li>
-					<li><a href = "#">Item 2</a></li>
-					<li><a href = "#">Item 3</a></li>
-					<li><a href = "#">Item 4</a></li>
-					<li><a href = "#">Item 5</a></li>
-				</ul>
-			</div>
-		
-			<div class= "col-md-2 quicklinklist">
-				<h4> Option 5</h4>
-				<ul>
-					<li><a href = "#">Item 1</a></li>
-					<li><a href = "#">Item 2</a></li>
-					<li><a href = "#">Item 3</a></li>
-					<li><a href = "#">Item 4</a></li>
-					<li><a href = "#">Item 5</a></li>
-				</ul>
-			</div>
-	  	</div><!--Inner Row  ended -->
+     	 <?php include('quicklinks.php') ?>
   	 </div><!--col-md-8 class ended -->
   
   	 <div class="col-md-3 testimonial text-center">
