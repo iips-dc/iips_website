f2c2a018eaf6d5a73b633c0a340f96e9728d4185
gauravparmar <gauravparmariam@gmail.com>
2013-11-27 12:15:48 +0530

    Fragmented header into 3 parts for ease in inclusion

---
+++ portals.php
@@ -0,0 +1,13 @@
+	<!-- Top nav bar -->
+	<div class="gateway">
+	  <div class="container" >
+	     <div class="col-md-12">
+			<div class="tab"> <a href="student_portal.php">Student</a> </div>
+			<div class="tab"> <a href="faculty_portal.php" >Faculty</a></div>
+			<div class="tab"> <a href="alumni_portal.php">Alumni</a></div>
+			<div class="tab"> <a href="staff_portal.php">Staff</a></div>
+			<div class="pull-right"> <a href="#" >Contact us</a></div>
+		  </div>
+	  </div>
+	</div>
+	<!-- Top nav bar end -->
