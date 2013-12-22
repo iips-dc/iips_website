4c1cdadc202b72f056923014f19d23b82c43e275
Rahul-Sagore <rahul.sagore@gmail.com>
2013-12-22 22:29:16 +0530

    Updated portals.php file with disabled alumni link

--- portals.php
+++ portals.php
@@ -5,7 +5,7 @@
 			<div class="tab"> <a href="index.php">Home</a> </div>
 			<div class="tab"> <a href="student_portal.php">Student</a> </div>
 			<div class="tab"> <a href="faculty_portal.php" >Faculty</a></div>
-			<div class="tab"> <a href="alumni_portal.php">Alumni</a></div>
+			<div class="tab"> Alumni</div>
 			<div class="tab"> <a href="staff_portal.php">Staff</a></div>
 			<div class="pull-right"> <a href="#" >Contact us</a></div>
 		  </div>
