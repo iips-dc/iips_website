e3b39bd1d501450921a68e4e8a47a4fd8578eff5
Rahul-Sagore <rahul.sagore@gmail.com>
2013-12-25 07:05:32 +0530

    Added files containing css/javascript for slider

---
+++ slider_jsLinks.php
@@ -0,0 +1,6 @@
+<script type="text/javascript" src="jsLibrary/jquery.nivo.slider.js"></script>
+	<script type="text/javascript">
+		$(window).load(function() {
+			$('#slider').nivoSlider();
+		});
+    </script>
