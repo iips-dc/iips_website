f761a6bc4067a1fd9bd2eb22a3f6826ca66ecb36
Ankit-Kulkarni <ankit.kul1890@gmail.com>
2013-12-28 09:50:21 +0530

    changed the names of the files in single format

---
+++ about_iips.php
@@ -0,0 +1,112 @@
+ <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+        <title>About IIPS</title>
+
+       <!-- Include Styles -->	
+        <!--[if IE 7]><style type="text/css">#v-nav>ul>li.current{border-right:1px solid #fff!important}#v-nav>div.tab-content{z-index:-1!important;left:0}</style><![endif]-->
+        <!--[if IE 8]><style type="text/css">#v-nav>ul>li.current{border-right:1px solid #fff!important}#v-nav>div.tab-content{z-index:-1!important;left:0}</style><![endif]-->
+		<?php
+		 include('cssLinks.php');
+		?>
+		<style type="text/css">
+			.iips_logo{
+				float:right;
+			}
+		</style>
+    </head>
+    <body>
+		<?php
+			include('header.php');
+			include('readTextFilesScript.php');
+				
+		?>
+	   <div id="wrap">
+		 <div class="container" style="background-color:#FFFFFF;">
+		   
+		   <br><h3> About IIPS </h3><br>
+		   <div class="row">
+              <div class="col-sm-2">
+					
+                  <ul class="nav side-tabs nav-pills">
+                   	 <li class="active"><a class="icon-chevron-sign-right" href="#tab1">  About IIPS</a></li>
+                     <li><a class="icon-chevron-sign-right" href="#tab2">  Director</a></li>
+                     <li><a class="icon-chevron-sign-right" href="#tab3">  Objectives and Vision</a></li>
+					  <li><a class="icon-chevron-sign-right" href="#tab4">  Academic Calendar</a></li>
+					  <li><a class="icon-chevron-sign-right" href="#tab5">  IIPS @ Wikipedia</a></li>
+                      <li><a class="icon-chevron-sign-right" href="#tab6">  Anti-Ragging Committee</a></li>
+               	   </ul>
+			  </div>
+			  
+			  <div class="col-sm-10">
+
+                <div id="tab1"  class="tab-content active">
+					<img class="iips_logo" src="images/iipslogo.jpg">
+                    <?php 
+						$aboutIIPS = "textFiles/About_IIPS/About_IIPS.txt";  
+						 readTextFiles($aboutIIPS);
+				  ?>
+                </div>
+
+                <div id="tab2" class="tab-content hide" style="text-align:justify; padding:0px 20px 0px 20px; margin-top:0px; line-height:1.5;">
+				<b>Meet the Director</b>
+               <table style="background-color:#eeeeee; border:1px solid gray;" > <tr>  <td> <i>
+
+					<?php 
+						$director="textFiles/About_IIPS/Director.txt";  
+						readTextFiles($director);
+				  ?>
+
+                </div>
+
+                <div id="tab3" class="tab-content hide">
+                   <b> What we Think</b><br><br>
+
+					<div style="background-color:#eeeeee;"><b>Objectives</b></div>
+						<ul type="disc" style="margin-left:50px;">
+                              <?php 
+									$objectives="textFiles/About_IIPS/objective.txt";  
+									readTextFiles($objectives);
+				  			  ?>
+                </div>
+				
+				<div id="tab4" class="tab-content hide">
+					<b>Academic calender</b>
+				</div>
+				
+				<div id="tab5" class="tab-content hide">
+					Wikipedia : <a href="http://en.wikipedia.org/wiki/International_Institute_of_Professional_Studies">IIPS WikiPage</a>
+				</div>
+				
+                <div id="tab6" class="tab-content hide">
+                    <b>IIPS Anti-Ragging Committee</b><br><br>
+					
+					<ul type="disc" style="margin-left:50px;">
+    					<?php 
+							$antiRagging="textFiles/About_IIPS/antiRaggingCommittee.txt";  
+							readTextFiles($antiRagging); 
+				  	  ?>
+					</ul>	
+                   
+                </div><!--.tab-content Ended -->
+
+             </div><!-- .col-md-10 ended-->
+
+		</div><!--.row ended -->
+		
+	</div><!--.container ended -->
+   </div><!--wrap id ended -->
+		<?php
+		     include('footer.php');
+			include('jsLinks.php');
+		?>
+
+		
+<!-- container 12 ended -->
+
+</body>
+</html>
---
+++ courses.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+        <title>Programs We Offer</title>
+
+       <!-- Include Styles -->
+        
+        <!--[if IE 7]><style type="text/css">#v-nav>ul>li.current{border-right:1px solid #fff!important}#v-nav>div.tab-content{z-index:-1!important;left:0}</style><![endif]-->
+        <!--[if IE 8]><style type="text/css">#v-nav>ul>li.current{border-right:1px solid #fff!important}#v-nav>div.tab-content{z-index:-1!important;left:0}</style><![endif]-->
+		<?php
+		 include('cssLinks.php');
+		?>
+</head>
+
+<body>
+	<?php
+			include 'header.php';
+			
+			include('readTextFilesScript.php');
+	?>
+
+	 <div id="wrap">
+	  <div class="container" style="background-color:#FFFFFF;">
+
+		<br><h3> Programs We Offer </h3><br>
+         <div id="row">
+		   <div class="col-md-2">
+            <ul class="nav side-tabs nav-pills">
+                <li><a class="icon-chevron-sign-right" href="#tab1">  Under Graduate</a></li>
+                <li><a class="icon-chevron-sign-right" href="#tab2">  Integrated</a></li>
+                <li><a class="icon-chevron-sign-right" href="#tab3">  Post Graduate</a></li>
+                <li><a class="icon-chevron-sign-right" href="#tab4">M.Phil </a></li>
+                <li><a class="icon-chevron-sign-right" href="#tab5">Ph.D.</a></li>
+				
+            </ul>
+		   </div>
+
+
+			
+		   <div class="col-md-10">
+		   		<div id="tab1" class="tab-content active">
+				    <?php 
+							$bcomHons ="textFiles/ProgramsWeOffer/bcom_hons.txt";  
+							readTextFiles($bcomHons);  
+				    ?><br><br><br>
+
+                </div>
+
+              <div id="tab2"  class="tab-content hide">
+				<b>Integrated Courses</b><br /><br />
+					<ul class="nav nav-tabs" id="myTab">
+  						<li class="active"><a href="#mca" data-toggle="tab">MCA</a></li>
+  						<li><a href="#mba" data-toggle="tab">MBA</a></li>
+						<li><a href="#mtech">M.Tech</a></li>
+  					</ul>
+					
+					<div class="tab-content">
+					  <div class="tab-pane active" id="mca">
+					       <?php 
+								$mca ="textFiles/ProgramsWeOffer/mca.txt";  
+								readTextFiles($mca);  
+				    		?><br><br><br>
+					  </div>
+ 					  <div class="tab-pane" id="mba"></b>
+					     <?php 
+								$mba ="textFiles/ProgramsWeOffer/mba.txt";  
+								readTextFiles($mba);  
+				    		?><br><br><br>
+					  </div>
+ 					  <div class="tab-pane" id="mtech">
+					          <?php 
+								$mTech ="textFiles/ProgramsWeOffer/mtech.txt";  
+								readTextFiles($mTech);  
+				    		?><br><br><br>
+					  		
+					  </div>
+					</div>
+                </div>
+
+                <div id="tab3" class="tab-content hide" style="text-align:justify; padding:0px 20px 0px 20px; margin-top:0px; line-height:1.5;">
+		   			<b>Post Graduate Courses</b><br><br>
+
+					<ul class="nav nav-tabs" id="myTab">
+  						<li class="active"><a href="#mbams" data-toggle="tab">MBA-MS</a></li>
+  						<li><a href="#mbata" data-toggle="tab">MBA-TA</a></li>
+						<li><a href="#mbaapr">MBA-APR</a></li>
+  					</ul>
+					<div class="tab-content">
+  						<div class="tab-pane active" id="mbams">
+						    <?php 
+								$mbMS ="textFiles/ProgramsWeOffer/mba_ms.txt";  
+								readTextFiles($mbMS); 
+				    		?><br><br><br>
+						    
+						</div>
+  						<div class="tab-pane" id="mbata">
+						      <?php 
+								$mbaTA ="textFiles/ProgramsWeOffer/mba_ta.txt";  
+								readTextFiles($mbaTA); 
+				    		?><br><br><br>
+								
+						</div>
+ 					    <div class="tab-pane" id="mbaapr">
+								<?php 
+									$mbaAPR ="textFiles/ProgramsWeOffer/mba_apr.txt";  
+									readTextFiles($mbaAPR);  
+				    			?><br><br><br>
+								
+						</div>
+					</div>
+          	  </div>
+
+                
+            </div><!--.col-md-10 -->
+         </div><!--.row class ended -->
+
+	</div><!-- End of container class -->
+   </div><!--End of Wrap id -->		
+		<?php
+		    include('footer.php');
+			include('jsLinks.php');
+		?>
+		<!-- Include Scripts for vertical tabs-->
+		<script type="text/javascript" src="js/jQuery-hashchange.js"></script>       
+        <script type="text/javascript" src="js/vtab-script.js"></script>
+		<script>
+		
+		</script>
+		
+</body>
+</html>
