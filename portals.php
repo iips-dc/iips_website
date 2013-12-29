805bb296c00f3610fc80731b26e63f6dd098302e
Rahul-Sagore <rahul.sagore@gmail.com>
2013-12-29 23:45:39 +0530

    Updated homepage link from index.php to index

--- portals.php
+++ portals.php
@@ -2,7 +2,7 @@
 	<div class="gateway downShadow">
 	  <div class="container" >
 	     <div class="col-md-12">
-			<div class="tab"><a href="index.php"><i class="icon-home"></i> Home</a> </div>
+			<div class="tab"><a href="index"><i class="icon-home"></i> Home</a> </div>
 			<div class="tab"><a href="student_portal.php">Student</a> </div>
 			<div class="tab"><a href="faculty_portal.php" ><i class="icon-male"></i> Faculty</a></div>
 			<div class="tab text-muted"> Alumni</div>
