f7ef9b8efab8f3369a1d5aa370807988394bb88d
Rahul-Sagore <rahul.sagore@gmail.com>
2013-12-30 16:01:18 +0530

    Added webpage of Syllabus

---
+++ syllabus.php
@@ -0,0 +1,237 @@
+ <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+        <title>IIPS - Syllabus</title>
+
+       <!-- Include Styles -->	
+        <!--[if IE 7]><style type="text/css">#v-nav>ul>li.current{border-right:1px solid #fff!important}#v-nav>div.tab-content{z-index:-1!important;left:0}</style><![endif]-->
+        <!--[if IE 8]><style type="text/css">#v-nav>ul>li.current{border-right:1px solid #fff!important}#v-nav>div.tab-content{z-index:-1!important;left:0}</style><![endif]-->
+		<?php
+		 include('cssLinks.php');
+		?>
+		<style type="text/css">
+			.margin-top{
+				margin-top:30px;
+			}
+		</style>
+    </head>
+    <body>
+		<?php
+			include('header.php');
+			include('readTextFilesScript.php');
+				
+		?>
+	   <div id="wrap">
+		 <div class="container" style="background-color:#FFFFFF;">
+		   
+		   <br><h3> Syllabus </h3><br>
+		   <div class="row">
+              <div class="col-sm-2">
+					
+                  <ul class="nav side-tabs nav-pills">
+                   	 <li class="active"><a class="icon-chevron-sign-right" href="#mca">  MCA-6Year</a></li>
+                     <li><a class="icon-chevron-sign-right" href="#mtech">  M.Tect(IT)</a></li>
+                     <li><a class="icon-chevron-sign-right" href="#mbaMS">  MBA(MS)</a></li>
+					  <li><a class="icon-chevron-sign-right" href="#mbaAPR">  MBA(APR)</a></li>
+					  <li><a class="icon-chevron-sign-right" href="#mbaTA">  MBA(TA)</a></li>
+                      <li><a class="icon-chevron-sign-right" href="#bCom">  B.Com(hons)</a></li>
+               	   </ul>
+			  </div>
+			  
+			  <div class="col-sm-10">
+
+                <div id="mca"  class="tab-content active">
+					<div class="row">
+						<div class="col-md-4 text-center">
+							Semester - I  - <a href="#">Download</a>
+						</div>
+						<div class="col-md-4 text-center">
+							Semester - II  - <a href="#">Download</a>
+						</div>
+						<div class="col-md-4 text-center">
+							Semester - III - <a href="#">Download</a>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-4 text-center margin-top">
+							Semester - IV - <a href="#">Download</a>
+						</div>
+						<div class="col-md-4 text-center margin-top">
+							Semester - V - <a href="#">Download</a>
+						</div>
+						<div class="col-md-4 text-center margin-top">
+							Semester - VI - <a href="#">Download</a>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-4 text-center margin-top">
+							Semester - VII - <a href="#">Download</a>
+						</div>
+						<div class="col-md-4 text-center margin-top">
+							Semester - VIII - <a href="#">Download</a>
+						</div>
+						<div class="col-md-4 text-center margin-top">
+							Semester - IX - <a href="#">Download</a>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-4 text-center margin-top">
+							Semester - X - <a href="#">Download</a>
+						</div>
+						<div class="col-md-4 text-center margin-top">
+							Semester - XI - <a href="#">Download</a>
+						</div>
+						<div class="col-md-4 text-center margin-top">
+							Semester - XII - <a href="#">Download</a>
+						</div>
+					</div>
+                </div>
+
+                <div id="mtech" class="tab-content hide">
+					<div class="row">
+						<div class="col-md-4 text-center">
+							Semester - I  - <a href="#">Download</a>
+						</div>
+						<div class="col-md-4 text-center">
+							Semester - II  - <a href="#">Download</a>
+						</div>
+						<div class="col-md-4 text-center">
+							Semester - III - <a href="#">Download</a>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-4 text-center margin-top">
+							Semester - IV - <a href="#">Download</a>
+						</div>
+						<div class="col-md-4 text-center margin-top">
+							Semester - V - <a href="#">Download</a>
+						</div>
+						<div class="col-md-4 text-center margin-top">
+							Semester - VI - <a href="#">Download</a>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-4 text-center margin-top">
+							Semester - VII - <a href="#">Download</a>
+						</div>
+						<div class="col-md-4 text-center margin-top">
+							Semester - VIII - <a href="#">Download</a>
+						</div>
+						<div class="col-md-4 text-center margin-top">
+							Semester - IX - <a href="#">Download</a>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-2">
+						</div>
+						<div class="col-md-4 text-center margin-top">
+							Semester - X - <a href="#">Download</a>
+						</div>
+						<div class="col-md-4 text-center margin-top">
+							Semester - XI - <a href="#">Download</a>
+						</div>
+						<div class="col-md-2">
+						</div>
+					</div>
+                </div>
+
+                <div id="mbaMS" class="tab-content hide">
+					<div class="row">
+						<div class="col-md-4 text-center">
+							Semester - I  - <a href="#">Download</a>
+						</div>
+						<div class="col-md-4 text-center">
+							Semester - II  - <a href="#">Download</a>
+						</div>
+						<div class="col-md-4 text-center">
+							Semester - III - <a href="#">Download</a>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-4 text-center margin-top">
+							Semester - IV - <a href="#">Download</a>
+						</div>
+						<div class="col-md-4 text-center margin-top">
+							Semester - V - <a href="#">Download</a>
+						</div>
+						<div class="col-md-4 text-center margin-top">
+							Semester - VI - <a href="#">Download</a>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-4 text-center margin-top">
+							Semester - VII - <a href="#">Download</a>
+						</div>
+						<div class="col-md-4 text-center margin-top">
+							Semester - VIII - <a href="#">Download</a>
+						</div>
+						<div class="col-md-4 text-center margin-top">
+							Semester - IX - <a href="#">Download</a>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-4">
+						</div>
+						<div class="col-md-4 text-center margin-top">
+							Semester - X - <a href="#">Download</a>
+						</div>
+						<div class="col-md-4">
+						</div>
+					</div>
+                </div>
+				
+				<div id="mbaAPR" class="tab-content hide">
+					<div class="row">
+						<div class="col-md-1">
+						</div>
+						<div class="col-md-5 text-center">
+							Semester - I  - <a href="#">Download</a>
+						</div>
+						<div class="col-md-5 text-center">
+							Semester - II  - <a href="#">Download</a>
+						</div>
+						<div class="col-md-1">
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-1">
+						</div>
+						<div class="col-md-5 text-center margin-top">
+							Semester - III - <a href="#">Download</a>
+						</div>
+						<div class="col-md-5 text-center margin-top">
+							Semester - V - <a href="#">Download</a>
+						</div>
+						<div class="col-md-1">
+						</div>
+					</div>
+				</div>
+				
+				<div id="mbaTA" class="tab-content hide">
+					
+				</div>
+				
+                <div id="bCom" class="tab-content hide">
+                    
+                </div><!--.tab-content Ended -->
+
+             </div><!-- .col-md-10 ended-->
+
+		</div><!--.row ended -->
+		
+	</div><!--.container ended -->
+   </div><!--wrap id ended -->
+		<?php
+		     include('footer.php');
+			include('jsLinks.php');
+		?>
+
+		
+<!-- container 12 ended -->
+
+</body>
+</html>
