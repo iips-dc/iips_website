6e4fc2700484e4eef2ad06c6bf1e1947b11413c9
Chitrank-Dixit <chitrankdixit@gmail.com>
2014-01-02 10:15:35 +0530

    courses.php modified using base_template.php

--- courses.php
+++ courses.php
@@ -1,3 +1,4 @@
+<?php include 'base_template.php' ?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -5,24 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-        <title>Programs We Offer</title>
+        <title>Programs We Offer | IIPS</title>
 
-       <!-- Include Styles -->
-        
-        <!--[if IE 7]><style type="text/css">#v-nav>ul>li.current{border-right:1px solid #fff!important}#v-nav>div.tab-content{z-index:-1!important;left:0}</style><![endif]-->
-        <!--[if IE 8]><style type="text/css">#v-nav>ul>li.current{border-right:1px solid #fff!important}#v-nav>div.tab-content{z-index:-1!important;left:0}</style><![endif]-->
-		<?php
-		 include('cssLinks.php');
-		?>
+       
 </head>
 
 <body>
-	<?php
-			include 'header.php';
-			
-			include('readTextFilesScript.php');
-	?>
-
+	<?php startblock('content'); ?>
 	 <div id="wrap">
 	  <div class="container" style="background-color:#FFFFFF;">
 
@@ -120,16 +110,7 @@
 
 	</div><!-- End of container class -->
    </div><!--End of Wrap id -->		
-		<?php
-		    include('footer.php');
-			include('jsLinks.php');
-		?>
-		<!-- Include Scripts for vertical tabs-->
-		<script type="text/javascript" src="js/jQuery-hashchange.js"></script>       
-        
-		<script>
-		
-		</script>
+   <?php endblock(); ?>
 		
 </body>
 </html>
