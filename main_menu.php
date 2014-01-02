b29d3835523172bc3482d068d08092f8f95f8bbb
Ankit-Kulkarni <ankit.kul1890@gmail.com>
2014-01-03 01:15:30 +0530

    modifide the main menu

--- main_menu.php
+++ main_menu.php
@@ -14,104 +14,106 @@
        			  <div class="navbar-collapse collapse">
          			  <ul class="nav navbar-nav">
           		  		
-            			  <li class="dropdown">
-                             <a href="#" class="dropdown-toggle">About us<b class="caret"></b></a>
-                             <ul class="dropdown-menu">
-				  				<li><a href="about_university.php">About University</a></li>
-                    			<li><a href="about_iips.php">About IIPS</a></li>
-                    			<li><a href="#2">Life @campus</a></li>
-                    			<li><a href="infrastructure.php">Infrastructure</a></li>
-								<li><a href="gallery.php">Gallery</a></li>
-                    			<li><a href="Reach_Us.php">Reach us</a></li>
+              <!-- About us -->
+            	 <li class="dropdown">
+                  <a href="#" class="dropdown-toggle">About us<b class="caret"></b></a>
+                          <ul class="dropdown-menu">
+				  				           <li><a href="about_university.php">About University</a></li>
+                        			<li><a href="about_iips.php">About IIPS</a></li>
+                        			<li><a href="#2">Life @campus</a></li>
+                        			<li><a href="infrastructure.php">Infrastructure</a></li>
+              								<li><a href="gallery.php">Gallery</a></li>
+                        			<li><a href="Reach_Us.php">Reach us</a></li>
                   			  </ul>
-                		   </li>
-						   <li class="dropdown">
-                  			  <a href="#" class="dropdown-toggle">Programs<b class="caret"></b></a>
-                  			  <ul class="dropdown-menu">
-                 				  <li><a href="courses.php?t=1">Under Graduate</a></li>
-                          <li><a href="courses.php?t=2">Integrated (UG+PG) Masters</a></li>
-                          <li><a href="courses.php?t=3">Post Graduate</a></li>
-                          <li> <a href="courses.php?t=4">M.Phil Ph.D</a> </li>
-                          <li> <a href="courses.php?t=5">Ph.D</a> </li>
+               </li>
 
-                  			  </ul>
-                		   </li>
-						  <li class="dropdown">
-                   			<a href="#" class="dropdown-toggle">Admission<b class="caret"></b></a>
-				   			<ul class="dropdown-menu">
-				      			 <li><a href="admission.php">Entrance Exam - CET</a></li>
-                       			<li><a href="admission.php">Integrated</a></li>
-                       			<li><a href="admission.php">Under Graduate</a></li>
-					   			<li><a href="admission.php">Post Graduate</a></li>
-					  			 <li><a href="admission.php">How to apply</a></li>
-					  			 <li><a href="admission.php">Fees structure</a></li> 
-				   			</ul>
-				  		  </li>
-						<!-- academics -->
-            <li class="dropdown">
-                        <a href="#" class="dropdown-toggle">Academics<b class="caret"></b></a>
-                <ul class="dropdown-menu">
-                    <li><a href="#1">Academic Calender</a></li>
-                    <li><a href="syllabus.php">Syllabus</a></li>
-                    <li><a href="notice_announcement.php">Notices and Announcements</a></li> 
-                    <li><a href="news.php">News</a></li>
-                </ul>
+               <!-- Programs -->
+						   <li class="dropdown">
+                  <a href="#" class="dropdown-toggle">Programs<b class="caret"></b></a>
+                  	<ul class="dropdown-menu">
+                 				  <li><a href="courses.php">Under Graduate</a></li>
+                          <li><a href="courses.php">Integrated (UG+PG) Masters</a></li>
+                          <li><a href="courses.php">Post Graduate</a></li>
+                          <li> <a href="courses.php">M.Phil Ph.D</a> </li>
+                          <li> <a href="courses.php">Ph.D</a> </li>
+                  	</ul>
               </li>
 
-            <!-- research -->
-
-            <li class="dropdown">
-                   			<a href="#" class="dropdown-toggle">Research<b class="caret"></b></a>
-				   			<ul class="dropdown-menu">
-				      			 <li><a href="dc.php">Development Center</a></li>
-                     <li><a href="research_cell.php">Research Center</a></li>
-                     <li><a href="publication.php">Publications</a></li>
-				   			</ul>
+              <!-- Admissions -->
+						  <li class="dropdown">
+                <a href="#" class="dropdown-toggle">Admissions<b class="caret"></b></a>
+      				   			<ul class="dropdown-menu">
+      				      			 <li><a href="admission.php">Entrance Exam - CET</a></li>
+                           <li><a href="admission.php#tab2">Eligibility</a></li>
+                           <li><a href="admission.php#tab3">Criteria</a></li>
+      					   			   <li><a href="admission.php">Counseling</a></li>
+      					  			   <li><a href="admission.php">How to apply</a></li>
+      					  			   <li><a href="admission.php">Fees structure</a></li> 
+      				   			</ul>
 				  		</li>
 
-            <!-- events -->
+						<!-- Academics -->
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle">Academics<b class="caret"></b></a>
+                  <ul class="dropdown-menu">
+                      <li><a href="academic_calender.php">Academic Calender</a></li>
+                      <li><a href="syllabus.php">Syllabus</a></li>
+                      <li><a href="notice_announcement.php">Notices and Announcements</a></li> 
+                      <li><a href="news.php">News</a></li>
+                  </ul>
+              </li>
+
+            
+            <!-- Research -->
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle">Research<b class="caret"></b></a>
+    				   			<ul class="dropdown-menu">
+    				      			 <li><a href="dc.php">Development Center</a></li>
+                         <li><a href="research_cell.php">Research Center</a></li>
+                         <li><a href="publication.php">Publications</a></li>
+    				   			</ul>
+				  	</li>
+
+            <!-- Events -->
 						<li class="dropdown">
-                  		   <a href="#" class="dropdown-toggle">Events<b class="caret"></b></a>
+              <a href="#" class="dropdown-toggle">Events<b class="caret"></b></a>
 				   		   <ul class="dropdown-menu">
 				       		  <li><a href="Xpressions.php">Xpression</a></li>
-                       		  <li><a href="synergy.php">Synergy</a></li>
-                      		 <li><a href="Tourista.php">Tourista</a></li> 
+                    <li><a href="synergy.php">Synergy</a></li>
+                    <li><a href="Tourista.php">Tourista</a></li> 
 				   			</ul>
-						 </li>
+						</li>
 						 
-			<!-- RTI -->
 
-						 <li class="dropdown">
-                  		   <a href="#" class="dropdown-toggle">RTI<b class="caret"></b></a>
-				   		   <ul class="dropdown-menu">
-				       		  <li><a href="downloads.php">Press Release</a></li>
-								<li><a href="downloads.php">Downloads</a></li>
-								<li><a href="downloads.php">Faculty</a></li>
-								<li><a href="downloads.php">Student</a></li>
-								<li><a href="downloads.php">Library</a></li>
-										  
-				   			</ul>
-						 </li>
-						
-						
-            <!-- recruitments -->
-
-						<li class="dropdown">
-                   			<a href="#" class="dropdown-toggle">Recruitments<b class="caret"></b></a>
-				   			<ul class="dropdown-menu">
-				      			<li><a href="#1">Faculty</a></li>
-                       			<li><a href="#2">Staff</a></li>
-				   			</ul>
-				  		</li>
-
-           <!-- placements -->
-
+			     <!-- Placements -->
 						<li class="dropdown">
 							<a href="placements.php" class="dropdown-toggle"> Placements</a>
-						  
 						</li>
 
-          			  </ul>
+
+            <!-- Downloads -->
+            <li class="dropdown">
+                <a href="downloads.php" class="dropdown-toggle">Downloads</a>
+             </li>
+
+
+            <!-- More -->
+             <li class="dropdown">
+                <a href="#" class="dropdown-toggle">More<b class="caret"></b></a>
+                 <ul class="dropdown-menu">
+                    <li><a href="library.php">Library</a></li>
+                    <li><a href="faculty_profile.php">Faculty Profile</a></li>
+                    <li><a href="staff_profile.php">Staff Profile</a></li>
+                    <li><a href="student_verification.php">Student Verification</a></li>
+                    <li><a href="video_channel.php">Video Channel</a></li>
+                    <li><a href="recruitments.php">Recruitments @iips</a></li>
+                    <li><a href="right_to_information.php">RTI</a></li>
+
+                          
+                </ul>
+             </li>
+
+        </ul>
 		       	 </div><!--/.nav-collapse -->
           	</div><!--.container -->
         </div><!--navbar-inverse -->
