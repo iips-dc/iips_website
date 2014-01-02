21db227895aed411b313ff563a66240a8d1cd413
Rahul-Sagore <rahul.sagore@gmail.com>
2014-01-02 22:36:47 +0530

    Added Template Inhertiance

--- about_university.php
+++ about_university.php
@@ -1,93 +1,63 @@
-<?php include 'base_template.php' ?>
-<!doctype html>
-
-<html>
-
-    <head>
-
-        <meta charset="utf-8" />
-
-        <title>About University | IIPS</title>
+<?php include 'base_template_2_column.php' ?>
 	
- 		
+	<?php startblock('page_heading'); ?>
+		<h3> About University </h3>
+	<?php endblock(); ?>
+	
+	 <!-- Template Block for Sidebar Options -->
+	<?php startblock('sidemenu'); ?>
+	
+		<ul class="nav side-tabs nav-pills">
+			<li class="active btn-block"><a class="icon-chevron-sign-right" href="#tab1"> About University</a></li>
+			<li class="btn-block"><a class="icon-chevron-sign-right" href="#tab2"> Vice Chancellor</a></li>
+			<li class="btn-block"><a class="icon-chevron-sign-right" href="#tab3"> Department</a></li>
+			<li class="btn-block"><a class="icon-chevron-sign-right" href="#tab4"> University Auditorium</a></li>
+			<li class="btn-block"><a class="icon-chevron-sign-right" href="#tab5"> IT Center</a></li>
+		
+		</ul>
+	<?php endblock(); ?>
+		
+	<!-- Template Block for Sidebar Content -->
+	<?php startblock('content'); ?>
+		<div id="tab1"  class="tab-content active">
+		  <?php 
+			$abtUniv ="textFiles/About_University/about_University.txt";  
+			readTextFiles($abtUniv);
+		 ?>
+		</div>
 
-       
-    </head>
+		<div id="tab2" class="tab-content hide">
+			<?php 
+				$viceChancellor ="textFiles/About_University/vice_Chancellor.txt";  
+				readTextFiles($viceChancellor);  
+			?>
+		</div>
 
-    <body>
-		<?php startblock('content'); ?>
- 		<div id="wrap">
-	        <div class="container" style="background-color:#FFFFFF;">
-			   <div class="row">
-			      <br><h3 class="title">Devi Ahilya University</h3><br>
-			   	  <div class="col-md-3">
-					<ul class="nav side-tabs nav-pills">
-	   					<li class="active btn-block"><a class="icon-chevron-sign-right" href="#tab1"> About University</a></li>
-	    				<li class="btn-block"><a class="icon-chevron-sign-right" href="#tab2"> Vice Chancellor</a></li>
-	    				<li class="btn-block"><a class="icon-chevron-sign-right" href="#tab3"> Department</a></li>
-	    				<li class="btn-block"><a class="icon-chevron-sign-right" href="#tab4"> University Auditorium</a></li>
-	   					<li class="btn-block"><a class="icon-chevron-sign-right" href="#tab5"> IT Center</a></li>
-	    			
-					</ul>
-	         		
-				  </div><!--.col-md-3 End -->
-	 
+		<div id="tab3" class="tab-content hide">
 
-	                <div class="col-md-9">
-						<div id="tab1"  class="tab-content active">
-	                      <?php 
-							$abtUniv ="textFiles/About_University/about_University.txt";  
-							readTextFiles($abtUniv);
-					     ?>
-	                   </div>
+		   <?php 
+				$departments ="textFiles/About_University/departments.txt";  
+				readTextFiles($departments);  
+			?>              
 
-	              <div id="tab2" class="tab-content hide">
-						<?php 
-							$viceChancellor ="textFiles/About_University/vice_Chancellor.txt";  
-							readTextFiles($viceChancellor);  
-					    ?>
-	                  
+		</div>
+				
+		<div  id="tab4" class="tab-content hide">
 
-	                </div>
+			<?php 
+				$univAuditorium ="textFiles/About_University/univ_Auditorium.txt";  
+				readTextFiles($univAuditorium); 
+			?> 
+			
+		</div>
 
-             
-	                <div id="tab3" class="tab-content hide">
+		<div  id="tab5" class="tab-content hide">
+			<?php 
+				$itCenter ="textFiles/About_University/itCenter.txt";  
+				readTextFiles($itCenter); 
+			?>
+			
+		</div>
+   <?php endblock(); ?>
 
-	                   <?php 
-							$departments ="textFiles/About_University/departments.txt";  
-							readTextFiles($departments);  
-					    ?>              
 
-	                </div>
-
-	 				
-	                <div  id="tab4" class="tab-content hide">
-
-	                    <?php 
-							$univAuditorium ="textFiles/About_University/univ_Auditorium.txt";  
-							readTextFiles($univAuditorium); 
-					    ?> 
-	                    
-	                </div>
-
-					<div  id="tab5" class="tab-content hide">
-						<?php 
-							$itCenter ="textFiles/About_University/itCenter.txt";  
-							readTextFiles($itCenter); 
-					    ?>
-	                    
-	                </div>
-				</div><!--.col-md-9 end -->
-			  </div><!--.row class ended -->
-			</div>  
-
-       </div><!-- End of container class -->
-       <?php endblock(); ?>
-        <!-- Include Scripts -->
-
-      
- 
-
-    </body>
-
-</html>
--- gallery.php
+++ gallery.php
@@ -1,94 +1,38 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyDY0kkJiTPVd2U7aTOAwhc9ySH6oHxOIYM&sensor=false">
-	</script>
+<?php include 'base_template_2_column.php' ?>
 
-	<script>
-	var myCenter=new google.maps.LatLng(22.68849,75.87669);
-	var marker;
+	<!-- Starting of block for page heading -->
+	<?php startblock('page_heading'); ?>
+		<h3> IIPS Gallery </h3>
+	<?php endblock(); ?>
+	
+	<!-- Starting of block for Sidebar Options -->
+	<?php startblock('sidemenu'); ?>
 
-	function initialize()
-	{
-	var mapProp = {
-	center:myCenter,
-	zoom: 15,
-	mapTypeId:google.maps.MapTypeId.ROADMAP
-	};
+		  <ul class="nav side-tabs nav-pills">
+			 <li class="active btn-block"><a class="icon-chevron-sign-right" href="#tab1">  Photos</a></li>
+			 <li class="btn-block"><a class="icon-chevron-sign-right" href="#tab2">  Animation</a></li>
+		   </ul>
+	<?php endblock() ; ?>
+	
+	<!-- Starting of block for Sidebar Content -->
+	<?php startblock('content') ?>			  	  
 
-	var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+		<div id="tab1"  class="tab-content active">
+			<b>Photos</b><br><br />
+			<img src="images/lab.jpg">
+			<img src="images/iips7.jpg">
+		</div>
 
-	marker=new google.maps.Marker({
-	position:myCenter,
-	animation:google.maps.Animation.BOUNCE
-	});
+		<div id="tab2" class="tab-content hide" style="text-align:justify; padding:0px 20px 0px 20px; margin-top:0px; line-height:1.5;">
+			<b>Animation</b><br><br>
+			<img src="images/slider/iips6.jpg">
+			<img src="images/slider/iips1.jpg">
+			<img src="images/slider/iips2.jpg">
+			<img src="images/slider/iips8.jpg">
+			<img src="images/slider/iips5.jpg">
 
-	marker.setMap(map);
-	}
+		</div>
 
-	google.maps.event.addDomListener(window, 'load', initialize);
-
-
-</script>
-       
-        <title>Reach Us</title>
-
-       <!-- Include Styles -->
-        <link rel="stylesheet" href="css/vtab-style.css" />		
-        <!--[if IE 7]><style type="text/css">#v-nav>ul>li.current{border-right:1px solid #fff!important}#v-nav>div.tab-content{z-index:-1!important;left:0}</style><![endif]-->
-        <!--[if IE 8]><style type="text/css">#v-nav>ul>li.current{border-right:1px solid #fff!important}#v-nav>div.tab-content{z-index:-1!important;left:0}</style><![endif]-->
-		<?php
-		 include('cssLinks.php');
-		?>
-</head>
-
-<body>
-	<?php
-			include('header.php');
-	?>
-	 <div id="wrap">
-	  <div class="container" style="background-color:#FFFFFF;">
-	   	  <h4>Reach Us</h4><br><br>
-		  
- 		  <div class="row">
-		     <div class="col-md-4" style="background-color:#333333; color:#FFFFFF">
-	 
-				<b>IIPS Address : </b><br><br>
-				Devi Ahilya University<br>
-					Takshashila Campus, Khandwa Road<br>
-					Indore - 452001, INDIA<br>
-					Phone: 91-731-2461888, 2462087, 2461332, 2760101<br>
-					Fax - 91-731-2467888<br>
-					admin@iips.edu.in<br>
-					*for more details go through our virtual tour.<br>
-             </div>
-
-             <div class="col-md-8">
-                   <b>Virtual Tour</b><br><br>
-					<!-- Google Maps iframe link -->
-					<div id="googleMap" style="width:500px;height:380px;"></div>
-					<center>
-					<input type="button" value="Guide Me" onClick=""></input>
-					<input type="button" value="Pause" onClick=""></input>
-					<input type="button" value="Manual Navigation" onClick=""></input>
-					<input type="button" value="Previous" onClick=""></input>
-					<input type="button" value="Next" onClick=""></input>
-					
-					</center>
-             </div>
-		  </div><!--row class Ended -->
-       </div><!--container class Ended -->
-	</div><!-- End of wrap class -->		
-		<?php
-		include('footer.php');
-			include('jsLinks.php');
-		?>
-		<!-- Include Scripts for vertical tabs-->
-		<script type="text/javascript" src="js/jQuery-hashchange.js"></script>       
-        <script type="text/javascript" src="js/vtab-script.js"></script>
-</body>
-</html>
+		      
+            
+   <?php endblock(); ?>
