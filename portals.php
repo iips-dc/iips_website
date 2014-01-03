b65ef6cc9e05e1ef72e2efba1454b9de482153c0
Rahul-Sagore <rahul.sagore@gmail.com>
2014-01-04 03:51:33 +0530

    Added Navbar-default

--- portals.php
+++ portals.php
@@ -1,14 +1,51 @@
 	<!-- Top nav bar -->
-	<div class="gateway downShadow">
-	  <div class="container" >
-	     <div class="col-md-12">
-			<div class="tab"><a href="index.php"><i class="icon-home"></i> Home</a> </div>
-			<div class="tab"><a href="student_portal.php">Student</a> </div>
-			<div class="tab"><a href="faculty_portal.php" ><i class="icon-male"></i> Faculty</a></div>
-			<div class="tab text-muted"> Alumni</div>
-			<div class="tab"> <a href="staff_portal.php">Staff</a></div>
-			<div class="pull-right"> <a href="#" >Contact us</a></div>
-		  </div>
-	  </div>
+
+	<div class="navbar-wrapper" >
+        <div class="navbar navbar-default  downShadow">
+		    <div class="container">
+       			<div class="navbar-header">
+         			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="icon-bar"></span>
+            		 	<span class="icon-bar"></span>
+           			 	<span class="icon-bar"></span>
+         			</button>
+       			</div>
+       			<div class="navbar-collapse collapse">
+         		    <ul class="nav navbar-nav">
+          		    
+						<!-- Home -->
+						<li class="dropdown">
+							<a href="index" class="icon-home"> Home</a>
+						</li>
+						
+						<!-- Student -->
+						<li class="dropdown">
+							<a href="#" class="icon-male"> Student</a>
+						</li>
+
+						<!-- Faculty -->
+						<li class="dropdown">
+							<a href="#" class="icon-male"> Faculty</a>
+						</li>
+
+						<!-- Alumni -->
+						<li class="disabled">
+							<a href="#" class="icon-male"> Alumni</a>
+						</li>
+
+						<!-- Academics -->
+						<li class="dropdown">
+							<a href="#" class="icon-male"> Staff</a> 
+						</li>
+	
+				    </ul>
+					<ul class="nav navbar-nav navbar-right"> 
+						<li><a href="#" class="">Contact us</a></li>
+				    </ul>
+		       	</div><!--/.nav-collapse -->
+          	</div><!--.container -->
+        </div><!--navbar-inverse -->
 	</div>
-	<!-- Top nav bar end -->
+	<!-- navbar-wrapper end" -->
+	
+<!-- Top nav bar end -->
