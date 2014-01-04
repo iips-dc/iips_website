9cc5dff911631d0b0138fecb406a80b8c5ee7c6e
Rahul-Sagore <rahul.sagore@gmail.com>
2014-01-04 17:26:07 +0530

    Added page of Website Development Team

---
+++ about_developers.php
@@ -0,0 +1,74 @@
+<?php include 'base_full_width.php' ?>
+     
+	<?php startblock('page_title'); ?>
+		About Developers
+	<?php endblock(); ?>
+	 
+	<?php startblock('content'); ?>
+    <div class="row">
+      
+        <div class="col-lg-12">
+			<h1 class="page-header">About Us <small>It's Nice to Meet You!</small></h1>
+			<p>This is a great place to start off with a short and sweet description of your company, organization, or whatever purpose your website is serving. Keep it friendly, engaging, but short enough to where you won't lose your reader!</p>
+			<p>If you need a bit more space to describe what is going on, we recommend putting a picture in this section. Use the <code>pull-right</code> class on the image to make it look good!</p>
+        </div>
+
+        <div class="col-lg-12">
+          <h2 class="page-header">Our Team</h2>
+        </div>
+
+    </div>
+
+      <div class="row">
+        <div class="col-lg-4 col-sm-6">
+                <img class="img-circle img-responsive" src="http://placehold.it/200x200">
+            <h3>Ankit Kulkarni <small>Job Title</small></h3>
+            <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+                <img class="img-circle img-responsive" src="images/Dc/rahul-sagore.jpg">
+            <h3>Rahul Sagore <small>Job Title</small></h3>
+            <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+                <img class="img-circle img-responsive" src="http://placehold.it/200x200">
+            <h3>Chitrank Dixit <small>Job Title</small></h3>
+            <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+                <img class="img-circle img-responsive" src="http://placehold.it/200x200">
+            <h3>Sunny Raikwar <small>Job Title</small></h3>
+            <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+                <img class="img-circle img-responsive" src="http://placehold.it/200x200">
+            <h3>Shefali Agrawal<small>Job Title</small></h3>
+            <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+                <img class="img-circle img-responsive" src="http://placehold.it/200x200">
+            <h3>Nitesh Mishra<small>Job Title</small></h3>
+            <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+        </div>
+		<div class="col-lg-4 col-sm-6">
+                <img class="img-circle img-responsive" src="http://placehold.it/200x200">
+            <h3>Ajeet Khan <small>Job Title</small></h3>
+            <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+                <img class="img-circle img-responsive" src="http://placehold.it/200x200">
+            <h3>Akanksha Rahore <small>Job Title</small></h3>
+            <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+                <img class="img-circle img-responsive" src="http://placehold.it/200x200">
+            <h3>Bedi Singh Yadav <small>Job Title</small></h3>
+            <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+        </div>
+		<div class="col-lg-4 col-sm-6">
+                <img class="img-circle img-responsive" src="http://placehold.it/200x200">
+            <h3>Misha Mathai <small>Job Title</small></h3>
+            <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+        </div>
+      </div>
+<?php endblock(); ?>
