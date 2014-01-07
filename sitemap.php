0b7260fd5c4babc5da0336a3bb1e69906389986f
Rahul-Sagore <rahul.sagore@gmail.com>
2014-01-07 17:42:59 +0530

    Added a file for sitemap - raw version

---
+++ sitemap.php
@@ -0,0 +1,69 @@
+<?php include 'base_full_width.php' ?>
+
+	<?php startblock('page_title'); ?>
+		Sitemap
+	<?php endblock(); ?>
+ 
+	 <?php startblock('page_heading'); ?> 
+		<center class="text-primary">Sitemap</center>
+	<?php endblock(); ?>
+
+	 
+	<?php startblock('content'); ?>
+	<b class="text-success">About Us</b> <b class="caret"></b><br>
+		<a href="about_university">About University</a><br>
+		<a href="about_iips">About IIPS</a><br>
+		<a class="disabled" href="#">Life @campus</a><br>
+		<a href="infrastructure">Infrastructure</a><br>
+		<a href="gallery">Gallery</a><br>
+		<a href="Reach_Us">Reach us</a><br><br>
+	
+	<b class="text-success">Programs</b>	<b class="caret"></b><br>
+		<a href="courses">Under Graduate</a><br>
+		 <a href="courses">Integrated (UG+PG) Masters</a><br>
+		 <a href="courses">Post Graduate</a><br>
+	    <a href="courses">M.Phil Ph.D</a> <br>
+	    <a href="courses">Ph.D</a> <br><br>
+                  	
+	<b class="text-success">Admissions</b><b class="caret"></b><br>
+		<a href="admission">Entrance Exam - CET</a><br>
+	    <a href="admission">Eligibility</a><br>
+	    <a href="admission">Criteria</a><br>
+	   <a href="admission">Counseling</a><br>
+	   <a href="admission">How to apply</a><br>
+	   <a href="admission">Fees structure</a><br><br>
+		
+	<b class="text-success">Academics</b><b class="caret"></b><br>
+		<a href="academic_calender">Academic Calender</a><br>
+		<a href="syllabus">Syllabus</a><br>
+		<a href="notice_announcement">Notices and Announcements</a> <br>
+		<a href="news">News</a><br>
+
+            
+	<b class="text-success">Research</b><b class="caret"></b><br>
+		<a href="dc">Development Center</a><br>
+		<a href="research_cell">Research Center</a><br>
+		<a href="publication">Publications</a><br><br>
+						 
+	<b class="text-success">Events</b><b class="caret"></b><br>
+		<a href="Xpressions">Xpression</a><br>
+		<a href="synergy">Synergy</a><br>
+		<a href="Tourista">Tourista</a><br><br>
+					
+					
+	<b class="text-success">Placement</b><b class="caret"></b><br>	
+		<a href="placements"> Placements</a><br><br>
+		
+	<b class="text-success">Download</b><b class="caret"></b><br>
+        <a href="downloads">Downloads</a><br><br>
+             
+	<b class="text-success">More Options</b><b class="caret"></b><br>
+		<a href="library">Library</a><br>
+		<a href="faculty_profile">Faculty Profile</a><br>
+		<a href="staff_profile">Staff Profile</a><br>
+		<a href="student_verification">Student Verification</a><br>
+		<a href="video_channel">Video Channel</a><br>
+		<a href="recruitments">Recruitments @iips</a><br>
+		<a href="right_to_information">RTI</a><br>
+	<?php endblock(); ?>      
+	   
