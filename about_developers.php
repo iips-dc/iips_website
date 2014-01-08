a56534577c4760d53a65fdbce1ea84aa85bca01d
Rahul-Sagore <rahul.sagore@gmail.com>
2014-01-08 17:41:52 +0530

    Added Project Manager's content, Divided Team members

--- about_developers.php
+++ about_developers.php
@@ -10,7 +10,6 @@
         <div class="col-lg-12">
 			<h1 class="page-header">About Us <small>It's Nice to Meet You!</small></h1>
 			<p>This is a great place to start off with a short and sweet description of your company, organization, or whatever purpose your website is serving. Keep it friendly, engaging, but short enough to where you won't lose your reader!</p>
-			<p>If you need a bit more space to describe what is going on, we recommend putting a picture in this section. Use the <code>pull-right</code> class on the image to make it look good!</p>
         </div>
 
         <div class="col-lg-12">
@@ -18,8 +17,25 @@
         </div>
 
     </div>
-
-      <div class="row">
+	
+	
+	<div class="row">
+		<div class="col-sm-4">
+				<!-- Left empty with purpose -->
+		</div>
+		<div class="col-lg-4">
+				<img class="img-circle img-responsive" src="http://placehold.it/200x200">
+			<h3>Mr. Shaligram Prajapat <small class="text-primary"><b>Project Manager</b></small></h3>
+			<p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+		</div>
+		<div class="col-sm-4">
+				<!-- Left empty with purpose -->
+		</div>
+    </div>
+	
+	<h4 class="text-primary"><b>Core Developers</b></h3>
+	<hr>
+    <div class="row">
         <div class="col-lg-4 col-sm-6">
                 <img class="img-circle img-responsive" src="http://placehold.it/200x200">
             <h3>Ankit Kulkarni <small>Job Title</small></h3>
@@ -40,7 +56,12 @@
             <h3>Sunny Raikwar <small>Job Title</small></h3>
             <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
         </div>
-        <div class="col-lg-4 col-sm-6">
+	</div><br>
+	
+	<h4 class="text-primary"><b>Database Maintainers</b></h3> <hr>
+	
+	<div class="row">
+        <div class="col-lg-3 col-sm-6">
                 <img class="img-circle img-responsive" src="http://placehold.it/200x200">
             <h3>Shefali Agrawal<small>Job Title</small></h3>
             <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
@@ -55,6 +76,11 @@
             <h3>Ajeet Khan <small>Job Title</small></h3>
             <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
         </div>
+	</div><br>
+	
+	<h4 class="text-primary"><b>Website Maintainers & Testers</b></h3> <hr>
+	
+	<div class="row">
         <div class="col-lg-4 col-sm-6">
                 <img class="img-circle img-responsive" src="http://placehold.it/200x200">
             <h3>Akanksha Rahore <small>Job Title</small></h3>
@@ -70,5 +96,5 @@
             <h3>Misha Mathai <small>Job Title</small></h3>
             <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
         </div>
-      </div>
+    </div>
 <?php endblock(); ?>
