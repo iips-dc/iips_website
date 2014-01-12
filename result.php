57685821589cacafbb5433f4c5e734488a776b4c
Ankit-Kulkarni <ankit.kul1890@gmail.com>
2014-01-13 03:08:54 +0530

    modified results page to provide table views of results

--- result.php
+++ result.php
@@ -1,6 +1,6 @@
 <?php include 'base_template_2_column.php' ?>
 	<?php startblock('page_title'); ?>
-	result
+	Results | IIPS
 	<?php endblock(); ?>
 
       
@@ -23,159 +23,295 @@
 			                <li class="btn-block"><a class="icon-chevron-sign-right" href="#bcom"> B.Com Hons </a></li>
 			                <li class="btn-block"><a class="icon-chevron-sign-right" href="#mbaTA"> MBA TA 2 yrs</a></li>
 			                <li class="btn-block"><a class="icon-chevron-sign-right" href="#mbaAPR"> MBA APR 2yrs</a></li>
-			                <li class="btn-block"><a class="icon-chevron-sign-right" href="#mbaFT"> MBA FT 2yrs</a></li>
+			                <li class="btn-block"><a class="icon-chevron-sign-right" href="#mbaFT"> MBA MS 2yrs</a></li>
 						</ul>			
 			
   <?php endblock();?>
 	<?php startblock('page_heading'); ?>
-        RESULT JAN-MAY 2013
+	
+        <center>RESULT JAN-MAY 2013</center>
 	<?php endblock(); ?>
 	 
 	<?php startblock('content'); ?>
 
 						<div id="mca"  class="tab-content active">
 					            <div class="row margin-top">
-						            <div class="col-md-4 text-center">
-										Semester - II  - <a href="downloads/result/JAN-MAY2013/mca 6 yr ii sem.pdf">Download</a>
-									</div>
-									<div class="col-md-4 text-center">
-										Semester - IV - <a href="downloads/result/JAN-MAY2013/mca 6 yr iv sem.pdf">Download</a>
-									</div>
-									<div class="col-md-4 text-center ">
-										Semester - VI - <a href="downloads/result/JAN-MAY2013/mca 6 yr vi sem.pdf">Download</a>
-									</div>
-								</div>
-								<div class="row">
-									<div class="col-md-4 text-center margin-top">
-										Semester - VIII - <a href="downloads/result/JAN-MAY2013/mca 6 yr viii sem.pdf">Download</a>
-									</div>
-									<div class="col-md-4 text-center margin-top">
-										Semester - X - <a href="downloads/result/JAN-MAY2013/mca 6 yr x sem.pdf">Download</a>
-									</div>
-									<div class="col-md-4 text-center margin-top">
-										Semester - XII - <a href="downloads/result/JAN-MAY2013/mca 6 yr xii sem.pdf">Download</a>
-									</div>
+					            	<table align="center" border="1px" class="table" >
+					            	<tr style="color:#FFFFFF; background-color:#333333"> <td>Semesters</td>	<td>Download Links</td>	 </tr>
+						            	
+						            	<tr> 
+						            		<td>Semester - II </td>
+						            		<td> <a href="downloads/result/JAN-MAY2013/mca 6 yr ii sem.pdf">Download</a> </td>
+						            	</tr>
+						            	
+						            	<tr> 
+						            		<td> Semester - IV </td>
+						            		<td> <a href="downloads/result/JAN-MAY2013/mca 6 yr iv sem.pdf">Download</a> </td>
+						            	</tr>
+						            	
+						            	<tr> 
+						            		<td> Semester - VI </td>
+						            		<td> <a href="downloads/result/JAN-MAY2013/mca 6 yr vi sem.pdf">Download</a> </td>
+						            	</tr>
+
+									
+									<tr> 
+										<td>Semester - VIII</td>
+										<td> <a href="downloads/result/JAN-MAY2013/mca 6 yr viii sem.pdf">Download</a></td>
+									</tr>
+
+
+									<tr> 
+										<td>Semester - X  </td>
+										<td><a href="downloads/result/JAN-MAY2013/mca 6 yr x sem.pdf">Download</a> </td>
+									</tr>
+									
+									<tr> 
+										<td> Semester - XII </td>
+										<td> <a href="downloads/result/JAN-MAY2013/mca 6 yr xii sem.pdf">Download</a></td>
+									</tr>
+									</table>
+									
+																	
 								</div>
 					   
 	                   </div>
  						
 	                   <div id="mtech"  class="tab-content hide">
 	                      <div class="row margin-top">
-						            <div class="col-md-4 text-center">
-										Semester - II  - <a href="downloads/result/JAN-MAY2013/m.tech ii sem.pdf">Download</a>
-									</div>
-									<div class="col-md-4 text-center">
-										Semester - IV - <a href="downloads/result/JAN-MAY2013/m.tech iv sem.pdf">Download</a>
-									</div>
-									<div class="col-md-4 text-center ">
-										Semester - VI - <a href="downloads/result/JAN-MAY2013/m.tech vi sem.pdf">Download</a>
-									</div>
-								</div>
-								<div class="row">
-									<div class="col-md-4 text-center margin-top">
-										Semester - VIII - <a href="downloads/result/JAN-MAY2013/m.tech viii sem.pdf">Download</a>
-									</div>
-									<div class="col-md-4 text-center margin-top">
-										Semester - X - <a href="downloads/result/JAN-MAY2013/m.tech x sem.pdf">Download</a>
-									</div>
-									
-								</div>
-					   
-	                   </div>
+
+	                      	<table align="center" border="1px" class="table" >
+					            	<tr style="color:#FFFFFF; background-color:#333333"> <td>Semesters</td>	<td>Download Links</td>	 </tr>
+						            	
+
+	                      	<tr> 
+	                      		<td>Semester - II </td>
+	                      		<td><a href="downloads/result/JAN-MAY2013/m.tech ii sem.pdf">Download</a> </td>
+	                      	</tr>
+
+	                      	
+	                      	<tr> 
+	                      		<td>Semester - IV  </td>
+	                      		<td><a href="downloads/result/JAN-MAY2013/m.tech iv sem.pdf">Download</a> </td>
+	                      	</tr>
+	                      	
+	                      	<tr> 
+	                      		<td> Semester - VI</td>
+	                      		<td> <a href="downloads/result/JAN-MAY2013/m.tech vi sem.pdf">Download</a> </td>
+	                      	</tr>
+
+	                      	<tr> 
+	                      		<td>Semester - VIII </td>
+	                      		<td> <a href="downloads/result/JAN-MAY2013/m.tech viii sem.pdf">Download</a></td>
+	                      	</tr>
+
+	                      	
+	                      	<tr> 
+	                      		<td>Semester - X  </td>
+	                      		<td> <a href="downloads/result/JAN-MAY2013/m.tech x sem.pdf">Download</a></td>
+	                      	</tr>
+	                      	
+	                      	</table>
+
+	                      			     
+					   		</div>
+	                   	   </div>
 
 	                   <div id="mbaMS"  class="tab-content hide">
 	                      <div class="row margin-top">
-						            <div class="col-md-4 text-center">
-										Semester - II  - <a href="downloads/result/JAN-MAY2013/mba 5yr-iisem.pdf">Download</a>
-									</div>
-									<div class="col-md-4 text-center">
-										Semester - IV - <a href="downloads/result/JAN-MAY2013/mba 5 yr iv sem.pdf">Download</a>
-									</div>
-									<div class="col-md-4 text-center ">
-										Semester - VI - <a href="downloads/result/JAN-MAY2013/mba 5 yr vi sem.pdf">Download</a>
-									</div>
+
+	                      	<table align="center" border="1px" class="table" >
+	                      	<tr style="color:#FFFFFF; background-color:#333333"> <td>Semesters</td>	<td>Download Links</td>	 </tr>
+	                      		
+
+	                      	<tr> 
+	                      		<td> Semester - II  </td>
+	                      		<td> <a href="downloads/result/JAN-MAY2013/mba 5yr-iisem.pdf">Download</a></td>
+	                      	</tr>
+
+	                      	
+	                      	<tr> 
+	                      		<td> Semester - IV </td>
+	                      		<td> <a href="downloads/result/JAN-MAY2013/mba 5 yr iv sem.pdf">Download</a></td>
+	                      	</tr>
+	                      	
+	                      	<tr> 
+	                      		<td> Semester - VI </td>
+	                      		<td><a href="downloads/result/JAN-MAY2013/mba 5 yr vi sem.pdf">Download</a></td>
+	                      	</tr>
+
+	                      </table>
+
+						           
 								</div>
 								<div class="row">
-									<div class="col-md-4 text-center margin-top">
-										Semester - VIII HR- <a href="downloads/result/JAN-MAY2013/mba 5 yr 8 sem hr.pdf">Download</a>
-									</div>
-									<div class="col-md-4 text-center margin-top">
-										Semester - VIII  FINANCE- <a href="downloads/result/JAN-MAY2013/mba 5 yr 8 sem finance.pdf">Download</a>
-									</div>
-									<div class="col-md-4 text-center margin-top">
-										Semester - VIII  MARKETING- <a href="downloads/result/JAN-MAY2013/mba 5 yr 8 sem marketing.pdf">Download</a>
-									</div>
-								</div>
-								<div class="row">
+									<table align="center" border="1px" class="table" >
+									<tr style="color:#FFFFFF; background-color:#333333"> <td>Semesters</td>	<td>Download Links</td>	 </tr>
+										
+									<tr> 
+										<td> Semester - VIII HR</td>
+										<td> <a href="downloads/result/JAN-MAY2013/mba 5 yr 8 sem hr.pdf">Download</a> </td>
+									</tr>
+
 									
-									<div class="col-md-4 text-center margin-top">
-										Semester - X  HR- <a href="downloads/result/JAN-MAY2013/mba 5 yr 10 sem hr.pdf">Download</a>
-									</div>
-									<div class="col-md-4 text-center margin-top">
-										Semester - X FINANCE- <a href="downloads/result/JAN-MAY2013/mba 5 yr 10 sem finance.pdf">Download</a>
-									</div>
-									<div class="col-md-4 text-center margin-top">
-										Semester - X  MARKETING- <a href="downloads/result/JAN-MAY2013/mba 5 yr 10 sem markething.pdf">Download</a>
-									</div>
+									<tr> 
+										<td>Semester - VIII  FINANCE </td>
+										<td> <a href="downloads/result/JAN-MAY2013/mba 5 yr 8 sem finance.pdf">Download</a></td>
+									</tr>
+									
+									<tr> 
+										<td>Semester - VIII  MARKETING </td>
+										<td><a href="downloads/result/JAN-MAY2013/mba 5 yr 8 sem marketing.pdf">Download</a> </td>
+									</tr>
+
+								</table>
+
+								</div>
+								<div class="row">
+									<table align="center" border="1px" class="table" >
+									<tr style="color:#FFFFFF; background-color:#333333"> <td>Semesters</td>	<td>Download Links</td>	 </tr>
+										
+
+									<tr> 
+										<td>Semester - X  HR </td>
+										<td> <a href="downloads/result/JAN-MAY2013/mba 5 yr 10 sem hr.pdf">Download</a></td>
+									</tr>
+
+									
+									<tr> 
+										<td>Semester - X FINANCE </td>
+										<td><a href="downloads/result/JAN-MAY2013/mba 5 yr 10 sem finance.pdf">Download</a> </td>
+									</tr>
+									
+									<tr> 
+										<td> Semester - X  MARKETING</td>
+										<td> <a href="downloads/result/JAN-MAY2013/mba 5 yr 10 sem markething.pdf">Download</a> </td>
+									</tr>
+										
+									</table>
 								</div>
 								
 	                   </div> 
 
 	                   <div id="bcom"  class="tab-content hide">
+
+	                   	<table align="center" border="1px" class="table" >
+	                   	<tr style="color:#FFFFFF; background-color:#333333"> <td>Semesters</td>	<td>Download Links</td>	 </tr>
+	                   		
+
+	                   				<tr> 
+	                   					<td> Semester - II  </td>
+	                   					<td> <a href="downloads/result/JAN-MAY2013/b.com ii sem.pdf">Download</a></td>
+	                   				</tr>
+
+	                   				
+	                   				<tr> 
+	                   					<td>Semester - IV </td>
+	                   					<td><a href="downloads/result/JAN-MAY2013/b.com iv sem.pdf">Download</a> </td>
+	                   				</tr>
+	                   				
+	                   				<tr> 
+	                   					<td> Semester - VI </td>
+	                   					<td> <a href="downloads/result/JAN-MAY2013/b.com vi sem.pdf">Download</a> </td>
+	                   				</tr>
+
+	                   			</table>
+
 	                    <div class="row margin-top">
-						            <div class="col-md-4 text-center">
-										Semester - II  - <a href="downloads/result/JAN-MAY2013/b.com ii sem.pdf">Download</a>
-									</div>
-									<div class="col-md-4 text-center">
-										Semester - IV - <a href="downloads/result/JAN-MAY2013/b.com iv sem.pdf">Download</a>
-									</div>
-									<div class="col-md-4 text-center ">
-										Semester - VI - <a href="downloads/result/JAN-MAY2013/b.com vi sem.pdf">Download</a>
-									</div>
+						            
 						</div>
 	                   </div>
 
 	                   <div id="mbaTA"  class="tab-content hide">
+
+	                   	<table align="center" border="1px" class="table" >
+	                   	<tr style="color:#FFFFFF; background-color:#333333"> <td>Semesters</td>	<td>Download Links</td>	 </tr>
+	                   		
+
+	                   				<tr> 
+	                   					<td>Semester - II </td>
+	                   					<td> <a href="downloads/result/JAN-MAY2013/mba (ta) ii sem.pdf">Download</a> </td>
+	                   				</tr>
+
+	                   				
+	                   				<tr> 
+	                   					<td>Semester - IV  </td>
+	                   					<td><a href="downloads/result/JAN-MAY2013/mba (ta) iv sem.pdf">Download</a> </td>
+	                   				</tr>
+
+	                   		</table>
+	                   				
+						
 	                      <div class="row margin-top">
-						            <div class="col-md-4 text-center">
-										Semester - II  - <a href="downloads/result/JAN-MAY2013/mba (ta) ii sem.pdf">Download</a>
-									</div>
-									<div class="col-md-4 text-center">
-										Semester - IV - <a href="downloads/result/JAN-MAY2013/mba (ta) iv sem.pdf">Download</a>
-									</div>
+						            
 									
-						</div>
+						  </div>
 	                   </div>
 
 	                   <div id="mbaAPR"  class="tab-content hide">
+
+	                   		<table align="center" border="1px" class="table" >
+	                   		<tr style="color:#FFFFFF; background-color:#333333"> <td>Semesters</td>	<td>Download Links</td>	 </tr>
+	                   			
+	                   				<tr> 
+	                   					<td>Semester - II </td>
+	                   					<td> <a href="downloads/result/JAN-MAY2013/mba (apr) ii sem.pdf">Download</a></td>
+	                   				</tr>
+
+	                   				
+	                   				<tr> 
+	                   					<td>Semester - IV </td>
+	                   					<td> <a href="downloads/result/JAN-MAY2013/mba (apr) iv sem.pdf">Download</a> </td>
+	                   				</tr>
+	                   				
+	                   				<tr> 
+	                   					<td> </td>
+	                   					<td> </td>
+	                   				</tr>
+	                   			</table>
+												
 	                       <div class="row margin-top">
-						            <div class="col-md-4 text-center">
-										Semester - II  - <a href="downloads/result/JAN-MAY2013/mba (apr) ii sem.pdf">Download</a>
-									</div>
-									<div class="col-md-4 text-center">
-										Semester - IV - <a href="downloads/result/JAN-MAY2013/mba (apr) iv sem.pdf">Download</a>
-									</div>
+						           
 									
-						</div>
+							</div>
 	                   </div>
 
-	                   <div id="mbaFT"  class="tab-content hide">
+	                   <div id="mbaFT"  class="tab-content hide">					
+
+
 					   <div class="row margin-top">
-	                     <div class="col-md-4 text-center ">
-										Semester - II - <a href="downloads/result/JAN-MAY2013/mba 2 yr ii sem.pdf">Download</a>
-									</div>
-									<div class="col-md-4 text-center ">
-										Semester - IV HR- <a href="downloads/result/JAN-MAY2013/mba 2 yr 4 sem hr.pdf">Download</a>
-									</div>
-									<div class="col-md-4 text-center ">
-										Semester - IV  FINANCE- <a href="downloads/result/JAN-MAY2013/mba 2 yr 4 sem finance.pdf">Download</a>
-									</div>
-									</div>
-									<div class="row ">
-									<div class="col-md-4 text-center ">
-										Semester - IV MARKETING- <a href="downloads/result/JAN-MAY2013/mba 2 yr 4 sem marketing.pdf">Download</a>
-									</div>
-	                   </div>
+					   	                   	<table align="center" border="1px" class="table" >
+					   	                   	<tr style="color:#FFFFFF; background-color:#333333"> <td>Semesters</td>	<td>Download Links</td>	 </tr>
+					   	                   		
+
+
+					   	                   				<tr> 
+					   	                   					<td>Semester - II </td>
+					   	                   					<td> <a href="downloads/result/JAN-MAY2013/mba 2 yr ii sem.pdf">Download</a></td>
+					   	                   				</tr>
+
+					   	                   				
+					   	                   				<tr> 
+					   	                   					<td> Semester - IV HR</td>
+					   	                   					<td><a href="downloads/result/JAN-MAY2013/mba 2 yr 4 sem hr.pdf">Download</a> </td>
+					   	                   				</tr>
+					   	                   				
+					   	                   				<tr> 
+					   	                   					<td> Semester - IV  FINANCE-</td>
+					   	                   					<td> <a href="downloads/result/JAN-MAY2013/mba 2 yr 4 sem finance.pdf">Download</a> </td>
+					   	                   				</tr>
+
+					   	                   				<tr> 
+					   	                   					<td> Semester - IV MARKETING- </td>
+					   	                   					<td> <a href="downloads/result/JAN-MAY2013/mba 2 yr 4 sem marketing.pdf">Download</a></td>
+					   	                   				</tr>
+
+					   	                   
+					   						 </table>
+	                     
+									
+						</div>
+									
 					   </div>
 
 </div>
