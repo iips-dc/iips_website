b321bbc54f274795f90606ab086d6b6b53bf6d32
AkankshaRathore <akanksha.ashi94@gmail.com>
2014-01-16 18:14:48 +0530

    added the google analytics code

--- base_full_width.php
+++ base_full_width.php
@@ -87,6 +87,16 @@
 		<script type="text/javascript" src="js/jQuery-hashchange.js"></script>       
 
 	<!-- Script (JS files) block Start -->
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-46860115-2', 'redirectme.net');
+  ga('send', 'pageview');
+
+</script>
 	<?php startblock('script'); ?>
 		
 	<?php endblock(); ?>
