f018a7498803d5520409c76c1e475d443c98e486
AkankshaRathore <akanksha.ashi94@gmail.com>
2014-01-17 17:58:19 +0530

    added the modal window(subjectt- to upgrade browser to google chrome and firefox ) only for Internet Explorer

--- index.php
+++ index.php
@@ -20,12 +20,51 @@
   </head>
 
   <body>
+
+<!-- 
+  	<button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+  	  Launch demo modal
+  	</button> -->
+  	<!-- Button trigger modal -->
+  	<?php
+
+  	echo '
+
+  	
+
+  	<!-- Modal -->
+  	<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  	  <div class="modal-dialog">
+  	    <div class="modal-content">
+  	      <div class="modal-header">
+  	        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+  	        <h4 class="modal-title" id="myModalLabel">Please Upgrade your browser</h4>
+  	      </div>
+  	      <div class="modal-body">
+  	        Please use Google Chrome <a href="https://www.google.com/intl/en/chrome/browser/">( Download Chrome )</a> 
+  	        or Firefox <a href="http://www.mozilla.org/en-US/firefox/all/">( Download Firefox )</a>
+  	        for better experience. 
+  	      </div>
+  	      <div class="modal-footer">
+  	        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+  	        <button type="button" class="btn btn-primary">Save changes</button>
+  	      </div>
+  	    </div><!-- /.modal-content -->
+  	  </div><!-- /.modal-dialog -->
+  	</div><!-- /.modal -->
+
+  		' ;
+  	?>
+
      <?php
 	 	include('header.php');
 	 ?>
 	 <div id="wrap">
 	 <?php
+	 	$browser = get_browser(null, true) ;
+		$browser['browser'] ;
 		include('content.php');
+
 	 ?>
 	 </div>
 	 <?php
@@ -38,6 +77,12 @@
 	
 	<!-- Script for "News/Events" tab -->
 	<script src="assets/js/holder.js"></script>
+	<?php
+	$browser = get_browser(null, true) ;
+	$browser =  strtolower($browser['browser']) ; 
+
+	if ($browser == 'ie') {
+	echo "
 	<script>
  		 $(function () {
   			$('#myTab a').click(function (e) {
@@ -47,14 +92,50 @@
 
     	    $('#myTab a:first').tab('show');
  		 })
+		
+  		 
+
+  		 $(window).load(function(){
+  		     $('#myModal').modal('show');
+  		 });
+ 				
+ 				 
 	</script>
+	" ;
+		}
+
+	else {
+
+			echo "
+			<script>
+		 		 $(function () {
+		  			$('#myTab a').click(function (e) {
+		    			e.preventDefault();
+		  				$(this).tab('show');
+					})
+
+		    	    $('#myTab a:first').tab('show');
+		 		 })
+
+		 				 
+			</script>
+			" ;
+
+	}	
+
+
+	?>
 
   <!-- Script for testimonial -->
-	<script>
+
 	$(document).ready(function() {
+
             $('ul#quotes').quote_rotator();
             $('ul#button_quotes').quote_rotator();
+          
         });
+
+   
    </script>
   </body>
 </html>
