b609b13f1e0a2eddd544d1462bcefdb94dea3442
Ankit-Kulkarni <ankit.kul1890@gmail.com>
2014-01-28 08:14:24 +0530

    modified the about_developers page

--- about_developers.php
+++ about_developers.php
@@ -9,7 +9,7 @@
       
         <div class="col-lg-12">
 			<h1 class="page-header">About Us <small>It's Nice to Meet You!</small></h1>
-			<p>This is our development team, who have contributed to the developement of the IIPS website and made this possible to deploy this website within the time.</p>
+			<p>This is our development team, who have contributed to the developement of the IIPS website and made this possible to deploy this website within the time.  * The names below are in no particular order. *</p>
         </div>
 
         <div class="col-lg-12">
@@ -37,32 +37,35 @@
 	<hr>
     <div class="row">
         <div class="col-lg-4 col-sm-6">
-                <img class="img-circle img-responsive" src="images/DC/ankit.jpg">
-            <h3>Ankit Kulkarni <small>Job Title</small></h3>
-            <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
-        </div>
-        <div class="col-lg-4 col-sm-6">
                 <img class="img-circle img-responsive" src="images/DC/rahul-sagore.jpg">
             <h3>Rahul Sagore <small>Job Title</small></h3>
             <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
         </div>
+
         <div class="col-lg-4 col-sm-6">
                 <img class="img-circle img-responsive" src="http://www.gravatar.com/avatar/143615a20f580213f50d651cf5148d11?d=mm&s=200">
             <h3>Chitrank Dixit <small>Job Title</small></h3>
             <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
         </div>
+
         <div class="col-lg-4 col-sm-6">
-                <img class="img-circle img-responsive" src="http://www.gravatar.com/avatar/b88ad6f999be3f82efbed2d0faea48ff?d=mm&s=200" >
-            <h3>Gaurav Parmar <small>Web Developer</small></h3>
-            <!--<p>Worked as a Web Developer.</p>-->
-            <p>Catch me on : <br><a href="http://being-technical.blogspot.com">My Blog</a> | <a href="http://linkedin.com/in/gauravbparmar">Linkedin</a>| <a href="http://twitter.com/gauravonquest">Twitter</a> | <a href="https://www.facebook.com/gauravparmariam">Facebook</a></p>
+                <img class="img-circle img-responsive" src="images/DC/ankit.jpg">
+            <h3>Ankit Kulkarni <small>Job Title</small></h3>
+            <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
         </div>
+
+
+    </div>
+       <br>
+
+    <div class="row">
         <div class="col-lg-4 col-sm-6">
                 <img class="img-circle img-responsive" src="images/DC/Sunny.jpg">
             <h3>Sunny Raikwar <small>Job Title</small></h3>
             <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
         </div>
-	</div><br>
+
+    </div>
 	
 	<h4 class="text-primary"><b>Database Maintainers</b></h3> <hr>
 	
@@ -87,23 +90,37 @@
 	<h4 class="text-primary"><b>Website Maintainers & Testers</b></h3> <hr>
 	
 	<div class="row">
+
+
+        <div class="col-lg-4 col-sm-6">
+                <img class="img-circle img-responsive" src="http://www.gravatar.com/avatar/b88ad6f999be3f82efbed2d0faea48ff?d=mm&s=200" >
+            <h3>Gaurav Parmar <small>Web Developer</small></h3>
+            <!--<p>Worked as a Web Developer.</p>-->
+            <p>Catch me on : <br><a href="http://being-technical.blogspot.com">My Blog</a> | <a href="http://linkedin.com/in/gauravbparmar">Linkedin</a>| <a href="http://twitter.com/gauravonquest">Twitter</a> | <a href="https://www.facebook.com/gauravparmariam">Facebook</a></p>
+        </div>
+
+        <div class="col-lg-4 col-sm-6">
+                <img class="img-circle img-responsive" src="images/DC/mishaMathai.jpg">
+            <h3>Misha Mathai <small>Job Title</small></h3>
+            <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+        </div>
+        
         <div class="col-lg-4 col-sm-6">
                 <img class="img-circle img-responsive" src="images/DC/Akanksha.jpg">
             <h3>Akanksha Rathore <small>Job Title</small></h3>
             <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
         </div>
+
+
+       
+
+    </div>
+	<div class="row">
         <div class="col-lg-4 col-sm-6">
                 <img class="img-circle img-responsive" src="images/DC/bedi.jpg">
             <h3>Bedi Singh Yadav <small>Job Title</small></h3>
             <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
         </div>
-		<div class="col-lg-4 col-sm-6">
-                <img class="img-circle img-responsive" src="images/DC/mishaMathai.jpg">
-            <h3>Misha Mathai <small>Job Title</small></h3>
-            <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
-        </div>
-    </div>
-	<div class="row">
 		<div class="col-lg-4 col-sm-6">
                 <img class="img-circle img-responsive" src="images/DC/komal.jpg">
             <h3>Komal Rathore <small>Job Title</small></h3>
