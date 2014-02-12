8bf3304a05ad5cc69d4b91b51382ccd761c0579f
gauravparmar <gauravparmariam@gmail.com>
2014-02-12 17:19:56 +0530

    Properly aligned dc page text

--- dc.php
+++ dc.php
@@ -44,7 +44,7 @@
 
 					<?php startblock('content'); ?>
 
-						<div id="tab1"  class="tab-content active">
+						<div id="tab1"  class="tab-content active" style="text-align:justify;">
 	                      <?php 
 							$aboutDC ="textFiles/DC/about_dc.txt";  
 							readTextFiles($aboutDC);
