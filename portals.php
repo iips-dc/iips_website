ef1f04265e08bc83355bb548a2f860ecdbf32e7b
gauravparmar <gauravparmariam@gmail.com>
2014-02-12 17:33:36 +0530

    Changed link text

--- portals.php
+++ portals.php
@@ -41,7 +41,7 @@
 				    </ul>-->
 					<!--<ul class="nav navbar-nav navbar-right"> -->
 
-						<a href="contact_us" class="pull-right" style="">Contact us</a>
+						<a href="contact_us" class="pull-right" style="">Contact Us</a>
 						<a href="https://docs.google.com/a/iips.edu.in/forms/d/1l3vtqwVc3oYDmqSayiJ-Txoc4wUP4DLfeZmfp8fwhtM/viewform" 
 						class="pull-right" style="padding-right: 15px;"> Feedback </a>
 				    <!--</ul>-->
