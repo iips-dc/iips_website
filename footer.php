3c60596ca097700e30cfe7d959122b1046539fd2
gaurav.parmar:q <gauravparmariam@gmail.com>
2014-02-18 00:42:28 +0530

    Added vertical lines to differentiate quicklinks and enhanced footer design

--- footer.php
+++ footer.php
@@ -1,22 +1,30 @@
-<footer style=" background-color:#000000; color:#ffffff;">	
-	<div class="container text-center" style="font-size:18px; padding-top:15px;">
-		<div class="row">
-	    	<div class="col-md-12">
+
+
+<footer style=" background-color:#2A3451; color:#ffffff;">	
+	<div class="text-center" >
+		<div  style="background-color:#777777;font-size:18px; padding-top:15px;padding-bottom:15px;">
+	    	
 				Stay Connected : 
 				<a href="https://plus.google.com/u/1/b/110977532784004536112/110977532784004536112/"  style="color:#ffffff;"><i class="icon-google-plus"></i></a>&nbsp;
 				<a href="https://www.facebook.com/iipsdavvofficial"  style="color:#ffffff;"><i class="icon-facebook"></i></a>&nbsp;
 				<a href="https://twitter.com/iipsdavv"  style="color:#ffffff;"><i class="icon-twitter"></i></a>&nbsp;
 				<a href="https://www.youtube.com/user/AdminIIPS/videos"  style="color:#ffffff;"><i class="icon-youtube"></i></a> 
 				
-			</div>
+			
 		</div><br>
+	</div>	
+	<div class="container text-center" style="font-size:18px;">
+		
 		<div class="row">
 	   	    <div class="col-md-12 text-center">
 	     		<?php include('quicklinks.php') ?><br>
 	  	    </div><!--col-md-8 class ended -->
 	    </div><!--row Class ended -->
 		
-		<div class="row">
+		
+		
+	</div>
+	<div class="text-center" style="font-size:18px; ">
 			
 			<div class="col-md-12">
 				<a href="dc"  style="color:#ffffff;font-size:18px;">Developed @ Developmet Center.</a><br>
@@ -25,8 +33,6 @@
 			</div>
 			
 		</div>
-		
-	</div>
 </footer>
 	<a href="#" class="go-top"><img src="images/up.png" height="45px"></a>
 
