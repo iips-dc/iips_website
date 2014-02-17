9b9a3df9aca96a6a458752f376a97f4621351a7f
gaurav.parmar:q <gauravparmariam@gmail.com>
2014-02-17 19:29:30 +0530

    Repositioned some footer links into quick links

--- quicklinks.php
+++ quicklinks.php
@@ -1,16 +1,12 @@
 <!-- this file is the quicklinks at the bottom of each page -->
 <!-- edit the options below to add the quick links -->
 	        <!-- testimonial started -->
-			<div class="panel panel-default  boxShadow" style="color:#2A3451;">
-				<div class="panel-heading">
-					<h3 class="panel-title">Quick Links</h3>
-				</div>
-				<div class="panel-body">
+	
 					<div class="row">
 						<div class="col-md-3">
 
-							<b> Admissions</b>
 							<ul style ="list-style:none;font-size: 12px;text-align:justify">
+								<font style="font-size:18px;"><b >Admissions</b></font>
 								<li> <a href="admission">CET</a></li>
 								<li> <a href="admission">Eligibility</a></li>
 								<li> <a href="admission">Counseling</a></li>
@@ -21,9 +17,9 @@
 						</div>
 						
 						<div class="col-md-3">
-							<b>Downloads</b>
 
 							<ul style ="list-style:none;font-size: 12px;text-align:justifyfont-size: 12px;text-align:justify">
+								<font style="font-size:18px;"><b >Downloads</b></font>
 								<li> <a href="academic_calender">Academic Calendar</a></li>
 								<li> <a href="syllabus.php">Syllabus</a></li>
 								<li> <a href="download">Formats</a></li>
@@ -32,38 +28,34 @@
 							
 							</ul>
 						</div>
-						<div class="col-md-3">
-							<b>Miscellaneous </b>
-
-							<ul style ="list-style:none;font-size: 12px;text-align:justify">
-								<li> <a href="faculty_profile">Faculty Profile</a></li>
-								<li> <a href="staff_profile">Staff Profile</a></li>
-								<li> <a href="student_verification">Student Verification</a></li>
-								<li> <a href="video_channel">Video Channel</a></li>
-								
-								
-								
-							</ul>
-						</div>
+						
 
 						<div class="col-md-3">
-							<b style ="padding-right: 55px;">	RTI</b>
-
 							<ul style ="list-style:none;font-size: 12px;text-align:justify">
+								<font style="font-size:18px;"><b >RTI</b></font>
 								<li> <a href="iqac">IQAC</a> </i>
 								<li> <a href="right_to_information">Press Release</a></li>
 								<li> <a href="right_to_information">Ordinances</a></li>
 								<li> <a href="recruitments">Recruitments</a></li>
 								<li> <a href="greviances">Greviances</a></li>
-
-							
+							</ul>
+						</div>
+						<div class="col-md-3">
+							<ul style ="list-style:none;font-size: 12px;text-align:justify">
+								<font style="font-size:18px;"><b >Miscellaneous</b></font>
+								<li> <a href="faculty_profile">Faculty Profile</a></li>
+								<li> <a href="staff_profile">Staff Profile</a></li>
+								<li> <a href="student_verification">Student Verification</a></li>
+								<li> <a href="video_channel">Video Channel</a></li>
+								<li> <a href="about_developers">About Developers</a></li>
+								<li> <a href="sitemap">Sitemap</a></li>
+								<li> <a href="https://docs.google.com/a/iips.edu.in/forms/d/1l3vtqwVc3oYDmqSayiJ-Txoc4wUP4DLfeZmfp8fwhtM/viewform">Feedback</a></li>
 							</ul>
 						</div>
 
 						
 					</div>
 				  
-				</div><!--panel body Ended -->
-			</div><!--Panel Ended -->
+
   	 
   
