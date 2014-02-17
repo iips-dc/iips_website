d7b9a527c7769b7f3f113133ca41b6b642e1e59a
gaurav.parmar:q <gauravparmariam@gmail.com>
2014-02-17 19:28:06 +0530

    Merged quick links in footer

--- footer.php
+++ footer.php
@@ -1,26 +1,29 @@
 <footer style=" background-color:#000000; color:#ffffff;">	
 	<div class="container text-center" style="font-size:18px; padding-top:15px;">
-		
 		<div class="row">
-			<div class="col-md-4">
-				IIPS, Davv, Takshila Campus<br />
-				Khandwa Road, Indore<br />
-				Indore, India, 452001
-			</div>
-			<div class="col-md-4">
-				<a href="about_developers" style="color:#ffffff;font-size:18px;">About Developers</a> &middot; <a href="sitemap"  style="color:#ffffff;font-size:18px;">Sitemap</a><br />
-				<a href="dc"  style="color:#ffffff;font-size:18px;">Developed @ Developmet Center</a><br>
-				Copyrights Reserved
-			</div>
-			<div class="col-md-4">
+	    	<div class="col-md-12">
 				Stay Connected : 
 				<a href="https://plus.google.com/u/1/b/110977532784004536112/110977532784004536112/"  style="color:#ffffff;"><i class="icon-google-plus"></i></a>&nbsp;
 				<a href="https://www.facebook.com/iipsdavvofficial"  style="color:#ffffff;"><i class="icon-facebook"></i></a>&nbsp;
 				<a href="https://twitter.com/iipsdavv"  style="color:#ffffff;"><i class="icon-twitter"></i></a>&nbsp;
 				<a href="https://www.youtube.com/user/AdminIIPS/videos"  style="color:#ffffff;"><i class="icon-youtube"></i></a> 
-				<br>
-				<a style = "margin-left : -46px; color:#ffffff; font-size:18px;" href="https://docs.google.com/a/iips.edu.in/forms/d/1l3vtqwVc3oYDmqSayiJ-Txoc4wUP4DLfeZmfp8fwhtM/viewform" >Feedback</a>
+				
 			</div>
+		</div><br>
+		<div class="row">
+	   	    <div class="col-md-12 text-center">
+	     		<?php include('quicklinks.php') ?><br>
+	  	    </div><!--col-md-8 class ended -->
+	    </div><!--row Class ended -->
+		
+		<div class="row">
+			
+			<div class="col-md-12">
+				<a href="dc"  style="color:#ffffff;font-size:18px;">Developed @ Developmet Center.</a><br>
+				Copyrights Reserved
+				<br>
+			</div>
+			
 		</div>
 		
 	</div>
