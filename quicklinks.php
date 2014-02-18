ed88dcffe1483dc8b02176c29d622f4eb454ac4f
gaurav.parmar:q <gauravparmariam@gmail.com>
2014-02-18 10:25:07 +0530

    Changed footer link color scheme

--- quicklinks.php
+++ quicklinks.php
@@ -1,26 +1,17 @@
 <!-- this file is the quicklinks at the bottom of each page -->
 <!-- edit the options below to add the quick links -->
 	        <!-- testimonial started -->
-<style type="text/css">
-	div div ul li a
-	{
-		color:white;
-	}
-	div div ul li a:hover
-	{
-		color:yellow;
-	}
-</style>	
+	
 					<div class="row">
 						<div class="col-md-3" style="border-left:2px solid white;">
 
-							<ul style ="list-style:none;font-size: 12px;text-align:justify">
-								<font style="font-size:18px;"><b >Admissions</b></font>
-								<li> <a href="admission">CET</a></li>
-								<li> <a href="admission">Eligibility</a></li>
-								<li> <a href="admission">Counseling</a></li>
-								<li> <a href="admission">Apply</a></li>
-								<li> <a href="admission">Fees Structure</a></li>
+							<ul style ="list-style:none;font-size: 12px;text-align:justify;">
+								<font style="font-size:18px;	"><b >Admissions</b></font>
+								<li> <a href="admission" style="color:white;">CET</a></li>
+								<li> <a href="admission" style="color:white;">Eligibility</a></li>
+								<li> <a href="admission" style="color:white;">Counseling</a></li>
+								<li> <a href="admission" style="color:white;">Apply</a></li>
+								<li> <a href="admission" style="color:white;">Fees Structure</a></li>
 							
 							</ul>
 						</div>
@@ -29,11 +20,11 @@
 
 							<ul style ="list-style:none;font-size: 12px;text-align:justifyfont-size: 12px;text-align:justify">
 								<font style="font-size:18px;"><b >Downloads</b></font>
-								<li> <a href="academic_calender">Academic Calendar</a></li>
-								<li> <a href="syllabus.php">Syllabus</a></li>
-								<li> <a href="download">Formats</a></li>
-								<li> <a href="download">Course PPTs</a></li>
-								<li> <a href="download">Miscellaneous</a></li>
+								<li> <a href="academic_calender" style="color:white;">Academic Calendar</a></li>
+								<li> <a href="syllabus.php" style="color:white;">Syllabus</a></li>
+								<li> <a href="download" style="color:white;">Formats</a></li>
+								<li> <a href="download" style="color:white;">Course PPTs</a></li>
+								<li> <a href="download" style="color:white;">Miscellaneous</a></li>
 							
 							</ul>
 						</div>
@@ -42,23 +33,23 @@
 						<div class="col-md-3" style="border-left:2px solid white;">
 							<ul style ="list-style:none;font-size: 12px;text-align:justify">
 								<font style="font-size:18px;"><b >RTI</b></font>
-								<li> <a href="iqac">IQAC</a> </i>
-								<li> <a href="right_to_information">Press Release</a></li>
-								<li> <a href="right_to_information">Ordinances</a></li>
-								<li> <a href="recruitments">Recruitments</a></li>
-								<li> <a href="greviances">Greviances</a></li>
+								<li> <a href="iqac" style="color:white;">IQAC</a> </i>
+								<li> <a href="right_to_information" style="color:white;">Press Release</a></li>
+								<li> <a href="right_to_information" style="color:white;">Ordinances</a></li>
+								<li> <a href="recruitments" style="color:white;">Recruitments</a></li>
+								<li> <a href="greviances" style="color:white;">Greviances</a></li>
 							</ul>
 						</div>
 						<div class="col-md-3" style="border-left:2px solid white;">
 							<ul style ="list-style:none;font-size: 12px;text-align:justify">
 								<font style="font-size:18px;"><b >Miscellaneous</b></font>
-								<li> <a href="faculty_profile">Faculty Profile</a></li>
-								<li> <a href="staff_profile">Staff Profile</a></li>
-								<li> <a href="student_verification">Student Verification</a></li>
-								<li> <a href="video_channel">Video Channel</a></li>
-								<li> <a href="about_developers">About Developers</a></li>
-								<li> <a href="sitemap">Sitemap</a></li>
-								<li> <a href="https://docs.google.com/a/iips.edu.in/forms/d/1l3vtqwVc3oYDmqSayiJ-Txoc4wUP4DLfeZmfp8fwhtM/viewform">Feedback</a></li>
+								<li> <a href="faculty_profile" style="color:white;">Faculty Profile</a></li>
+								<li> <a href="staff_profile" style="color:white;">Staff Profile</a></li>
+								<li> <a href="student_verification" style="color:white;">Student Verification</a></li>
+								<li> <a href="video_channel" style="color:white;">Video Channel</a></li>
+								<li> <a href="about_developers" style="color:white;">About Developers</a></li>
+								<li> <a href="sitemap" style="color:white;">Sitemap</a></li>
+								<li> <a href="https://docs.google.com/a/iips.edu.in/forms/d/1l3vtqwVc3oYDmqSayiJ-Txoc4wUP4DLfeZmfp8fwhtM/viewform" style="color:white;">Feedback</a></li>
 							</ul>
 						</div>
 
