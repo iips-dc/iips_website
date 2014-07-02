8d69bd3bd0e82e1bd3e2080fcc4e8a9a9dfbe619
Ankit-Kulkarni <ankit.kul1890@gmail.com>
2014-07-02 15:37:40 +0530

    added the information about 'developers' in about_developer page

--- about_developers.php
+++ about_developers.php
@@ -8,8 +8,8 @@
     <div class="row">
       
         <div class="col-lg-12">
-			<h1 class="page-header">About Us <small>It's Nice to Meet You!</small></h1>
-			<p>This is our development team, who have contributed to the developement of the IIPS website and made this possible to deploy this website within the time.  * The names below are in no particular order. *</p>
+			<h1 class="page-header">About Us <small>Here is the complete team behind this website </small></h1>
+			<p>This is our development team, who have contributed to the developement of the IIPS website and made this possible to deploy this website within the time. </p>
         </div>
 
         <div class="col-lg-12">
@@ -28,8 +28,8 @@
                 <img class="img-responsive" src="images/DC/shaligram.jpg">
 			<h3>Prof. Shaligram Prajapat <small class="text-primary"><b>Project Manager</b></small></h3>
             </center>
-			<p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
-            <center><p>Catch me on : </center><center><a href="https://sites.google.com/site/shaligramiipsdavvindore/">My Website</a> | <a href="#">Linkedin</a>| <a href="#">Twitter</a> | <a href="#">Facebook</a></p></center>
+			<p> </p>
+            <center><p>Catch me on : </center><center><a href="https://sites.google.com/site/shaligramiipsdavvindore/">My Website</a> | <a href="http://in.linkedin.com/in/shaligramprajapat8iips"> LinkedIn </a> | <a href="https://twitter.com/S_P_Indore">Twitter</a>   </p></center>
 		      
         </div>
 		<div class="col-sm-4">
@@ -40,26 +40,29 @@
 	<h4 class="text-primary"><b>Core Developers</b></h3>
 	<hr>
     <div class="row">
+
+    	<div class="col-lg-4 col-sm-6">
+                <img class="img-circle img-responsive" src="images/DC/ankit.jpg">
+            <h3>Ankit Kulkarni <small> Team Leader </small></h3>
+            <p> Logical System Design, Database design, backend coding, Documentation</p>
+            <p>Catch me on : <br> <a href="http://in.linkedin.com/pub/ankit-kulkarni/3b/455/552"> LinkedIn </a> | <a href="https://twitter.com/Ankitkul1890">Twitter</a> | <a href="https://github.com/Ankit-Kulkarni"> Github </a>  </p>
+        </div>
+
         <div class="col-lg-4 col-sm-6">
                 <img class="img-circle img-responsive" src="images/DC/rahul-sagore.jpg">
-            <h3>Rahul Sagore <small>Job Title</small></h3>
-            <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
-            <p>Catch me on : <br><a href="#">My Blog</a> | <a href="#">Linkedin</a>| <a href="#">Twitter</a> | <a href="#">Facebook</a></p>
+            <h3>Rahul Sagore <small> Lead Designer</small></h3>
+            <p> Front End development, feature development, backend coding, PBAS Developer </p>
+            <p>Catch me on : <br> <a href="http://lnkd.in/4VK_qz"> LinkedIn </a> | <a href="https://twitter.com/Rahulx1">Twitter</a> | <a href="https://github.com/Rahul-Sagore"> Github </a>  </p>
         </div>
 
         <div class="col-lg-4 col-sm-6">
                 <img class="img-circle img-responsive" src="http://www.gravatar.com/avatar/143615a20f580213f50d651cf5148d11?d=mm&s=200">
-            <h3>Chitrank Dixit <small>Job Title</small></h3>
-            <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
-            <p>Catch me on : <br><a href="#">My Blog</a> | <a href="#">Linkedin</a>| <a href="#">Twitter</a> | <a href="#">Facebook</a></p>
+            <h3>Chitrank Dixit <small>Lead Developer</small></h3>
+            <p> Feature development, backend coding, Technical Advisor </p>
+            <p>Catch me on : <br> <a href="http://in.linkedin.com/pub/chitrank-dixit/55/5/32/"> LinkedIn </a> | <a href="https://twitter.com/Chitrankdixit">Twitter</a> | <a href="https://github.com/Chitrank-Dixit"> Github </a>  </p>
         </div>
 
-        <div class="col-lg-4 col-sm-6">
-                <img class="img-circle img-responsive" src="images/DC/ankit.jpg">
-            <h3>Ankit Kulkarni <small>Job Title</small></h3>
-            <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
-            <p>Catch me on : <br><a href="#">My Blog</a> | <a href="#">Linkedin</a>| <a href="#">Twitter</a> | <a href="#">Facebook</a></p>
-        </div>
+        
 
 
     </div>
@@ -68,15 +71,15 @@
     <div class="row">
         <div class="col-lg-4 col-sm-6">
                 <img class="img-circle img-responsive" src="images/DC/Sunny.jpg" style="height:200px;">
-            <h3>Sunny Raikwar <small>Job Title</small></h3>
-            <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
-            <p>Catch me on : <br><a href="#">My Blog</a> | <a href="#">Linkedin</a>| <a href="#">Twitter</a> | <a href="#">Facebook</a></p>
+            <h3>Sunny Raikwar <small>Lead Tester and Documentation Engineer</small></h3>
+            <p> Front End development, backend coding , documentation </p>
+            <p>Catch me on : <br> <a href="http://in.linkedin.com/pub/sunny-raikwar/43/9aa/249"> LinkedIn </a> | <a href="https://twitter.com/SunnySraikwar">Twitter</a> | <a href="https://github.com/sunny-raikwar"> Github </a>  </p>
         </div>
         <div class="col-lg-4 col-sm-6">
                 <img class="img-circle img-responsive" src="http://www.gravatar.com/avatar/b88ad6f999be3f82efbed2d0faea48ff?d=mm&s=200" >
-            <h3>Gaurav Parmar <small>Web Developer</small></h3>
-            <!--<p>Worked as a Web Developer.</p>-->
-            <p>Catch me on : <br><a href="http://being-technical.blogspot.com">My Blog</a> | <a href="http://linkedin.com/in/gauravbparmar">Linkedin</a>| <a href="http://twitter.com/gauravonquest">Twitter</a> | <a href="https://www.facebook.com/gauravparmariam">Facebook</a></p>
+            <h3>Gaurav Parmar <small> Lead Designer</small></h3>
+            <p>Web Developer, Feature Developer</p>
+            <p>Catch me on : <br><a href="http://linkedin.com/in/gauravbparmar">Linkedin</a>| <a href="http://twitter.com/gauravonquest">Twitter</a> | <a href="https://github.com/gauravparmar"> Github </a></p>
         </div>
     </div>
 	
@@ -85,21 +88,21 @@
 	<div class="row">
         <div class="col-lg-3 col-sm-6">
                 <img class="img-circle img-responsive" src="images/DC/shaifali.jpg">
-            <h3>Shaifali Agrawal<small>Job Title</small></h3>
-            <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
-            <p>Catch me on : <br><a href="#">My Blog</a> | <a href="#">Linkedin</a>| <a href="#">Twitter</a> | <a href="#">Facebook</a></p>
+            <h3>Shaifali Agrawal <small> Database Engineer</small></h3>
+            <p> Database Migration and development </p>
+            <p>Catch me on : <br> <a href="http://in.linkedin.com/pub/shaifali-agrawal/72/414/699"> LinkedIn </a> | <a href="https://twitter.com/exploreshaifali">Twitter</a> | <a href="https://github.com/Shaifaliagrawal"> Github </a>  </p>
         </div>
         <div class="col-lg-4 col-sm-6">
                 <img class="img-circle img-responsive" src="images/DC/nitesh.jpg">
-            <h3>Nitesh Mishra<small>Job Title</small></h3>
-            <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
-            <p>Catch me on : <br><a href="#">My Blog</a> | <a href="#">Linkedin</a>| <a href="#">Twitter</a> | <a href="#">Facebook</a></p>
+            <h3>Nitesh Mishra <small> Database Engineer</small></h3>
+            <p> Database Migration, Pbas Developer </p>
+            <p>Catch me on : <br> <a href="http://in.linkedin.com/pub/nitesh-mishra/56/573/120/"> LinkedIn </a> | <a href="https://twitter.com/007niteshMishra">Twitter</a> | <a href="https://github.com/Nitesh-Mishra"> Github </a>  </p>
         </div>
 		<div class="col-lg-4 col-sm-6">
                 <img class="img-circle img-responsive" src="images/DC/ajeet.jpg">
-            <h3>Ajeet Khan <small>Job Title</small></h3>
-            <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
-            <p>Catch me on : <br><a href="#">My Blog</a> | <a href="#">Linkedin</a>| <a href="#">Twitter</a> | <a href="#">Facebook</a></p>
+            <h3>Ajeet Khan <small>Database Engineer</small></h3>
+            <p> Database Migration, Pbas Developer </p>
+            <p>Catch me on : <br> <a href="http://in.linkedin.com/pub/ajeet-khan/32/724/20b"> LinkedIn </a> | <a href="https://twitter.com/iajeetkhan">Twitter</a> | <a href="https://github.com/AjeetK"> Github </a>  </p>
         </div>
 	</div><br>
 	
@@ -111,16 +114,16 @@
 
         <div class="col-lg-4 col-sm-6">
                 <img class="img-circle img-responsive" src="images/DC/mishaMathai.jpg">
-            <h3>Misha Mathai <small>Job Title</small></h3>
-            <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
-            <p>Catch me on : <br><a href="#">My Blog</a> | <a href="#">Linkedin</a>| <a href="#">Twitter</a> | <a href="#">Facebook</a></p>
+            <h3>Misha Mathai <small>Design Engineer , Tester </small></h3>
+            <p> </p>
+            <p>Catch me on : <br> <a href="http://in.linkedin.com/pub/misha-mathai/56/540/476"> LinkedIn </a> | <a href="https://twitter.com/misha0612">Twitter</a> | <a href="https://github.com/misha-mathai"> Github </a>  </p>
         </div>
         
         <div class="col-lg-4 col-sm-6">
                 <img class="img-circle img-responsive" src="images/DC/Akanksha.jpg">
-            <h3>Akanksha Rathore <small>Job Title</small></h3>
-            <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
-            <p>Catch me on : <br><a href="#">My Blog</a> | <a href="#">Linkedin</a>| <a href="#">Twitter</a> | <a href="#">Facebook</a></p>
+            <h3>Akanksha Rathore <small>Mobile Apps Developer , Tester </small></h3>
+            <p> Site Maitainance, Testing </p>
+            <p>Catch me on : <br> <a href="http://www.linkedin.com/pub/akanksha-rathore/4b/73b/374"> LinkedIn </a> | <a href="https://twitter.com/AkankshaBlue25">Twitter</a> | <a href="https://github.com/AkankshaRathore/"> Github </a>  </p>
         </div>
 
 
@@ -130,15 +133,15 @@
 	<div class="row">
         <div class="col-lg-4 col-sm-6">
                 <img class="img-circle img-responsive" src="images/DC/bedi.jpg">
-            <h3>Bedi Singh Yadav <small>Job Title</small></h3>
-            <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
-            <p>Catch me on : <br><a href="#">My Blog</a> | <a href="#">Linkedin</a>| <a href="#">Twitter</a> | <a href="#">Facebook</a></p>
+            <h3>Bedi Singh Yadav <small>Mobile Apps Developer , Tester </small></h3>
+            <p> Site Maitainance, Testing</p>
+            <p>Catch me on : <br> <a href="http://www.linkedin.com/pub/bedi-singh-yadav/4b/746/1b0"> LinkedIn </a> | <a href="https://twitter.com/BediSinghYadav">Twitter</a> | <a href="https://github.com/Bedi-Singh-Yadav"> Github </a>   </p>
         </div>
 		<div class="col-lg-4 col-sm-6">
                 <img class="img-circle img-responsive" src="images/DC/komal.jpg">
-            <h3>Komal Rathore <small>Job Title</small></h3>
-            <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
-            <p>Catch me on : <br><a href="#">My Blog</a> | <a href="#">Linkedin</a>| <a href="#">Twitter</a> | <a href="#">Facebook</a></p>
+            <h3>Komal Rathore <small>Mobile Apps Developer , Tester</small></h3>
+            <p> </p>
+            <p>Catch me on : <br> <a href="http://in.linkedin.com/pub/komal-rathore/56/b97/3a7"> LinkedIn </a> | <a href="https://twitter.com/e_Komal">Twitter</a> | <a href="https://github.com/komal-rathore"> Github </a>  </p>
         </div>
 	</div>
 <?php endblock(); ?>
