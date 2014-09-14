f6823122eb2e5c722f71d4d9feca084441fb6a9c
Ankit-Kulkarni <ankit.kul1890@gmail.com>
2014-09-14 22:28:34 +0530

    added a notice for youth festival

--- content.php
+++ content.php
@@ -38,6 +38,7 @@ and testimonial section -->
 					<h3 class="panel-title">Notice & Announcements</h3>
 				</div>
 				<div class="panel-body " align="left">
+				  <a href="notice_announcement#youth_festival" style="color: green"> <i class="icon-pushpin"></i> UTD Level Selections for Youth Festival 2014-2015 </a><img src="images/new.gif" > <br> 
 				  <a href="notice_announcement#entrepreneurship_awareness" style="color: green"> <i class="icon-pushpin"></i> Entrepreneurship Awareness Drive 2014</a><img src="images/new.gif" > <br> 
 				  <a href="http://davvcet.in/results2.php"> <i class="icon-pushpin"></i> CET results announced </a> <br> 
 				  <a href="notice_announcement#exam_postpone"> <i class="icon-pushpin"></i> MBA-MS 5years II sem paper on 15/05/2014 postponed </a><br> 
