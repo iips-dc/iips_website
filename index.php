4858e263db0fbf6e0716319795033ba19f76c40c
gauravparmar <gauravparmariam@gmail.com>
2014-09-25 17:51:38 +0530

    Added author information for IIPS website in index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="author" content="Development Center - IIPS, Indore">
     <link rel="shortcut icon" href="images/favicon.png">
 
     <title>IIPS | HOME</title>
