c26d955b4b2afe0669ca9adbdb6414678b4daf39
Rahul-Sagore <rahul.sagore@gmail.com>
2014-11-18 12:22:40 +0530

    Removed buggy vode from the portals.php

--- portals.php
+++ portals.php
@@ -3,42 +3,33 @@
 	 <!--<div class="navbar-wrapper" >
         <div class="navbar navbar-default  downShadow">-->
 		    <div class="container">
-       			<!--<div class="navbar-header">
-         			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-						<span class="icon-bar"></span>
-            		 	<span class="icon-bar"></span>
-           			 	<span class="icon-bar"></span>
-         			</button>
-       			</div>
-       			<div class="navbar-collapse collapse">
-         		    <ul class="nav navbar-nav">
-          		    
+
 						<!-- Home -->
 						<!--<li class="dropdown"> -->
 							<a href="index" class="icon-home"> Home</a>
-						<!--</li>
+						<!--</li> -->
 						
 						<!-- Student -->
 						<!--<li class="dropdown">
 							<a href="#" class="icon-male"> Student</a>
-						</li>
+						</li>-->
 
 						<!-- Faculty -->
 						<!--<li class="dropdown">
 							<a href="#" class="icon-male"> Faculty</a>
-						</li>
+						</li>-->
 
 						<!-- Alumni -->
 						<!--<li class="disabled">
 							<a href="#" class="icon-male"> Alumni</a>
-						</li>
+						</li>-->
 
 						<!-- Academics -->
 						<!--<li class="dropdown">
 							<a href="#" class="icon-male"> Staff</a> 
-						</li>
+						</li>-->
 	
-				    </ul>-->
+				    <!--</ul>-->
 					<!--<ul class="nav navbar-nav navbar-right"> -->
 
 						<a href="contact_us" class="pull-right" style="">Contact us</a>
@@ -47,8 +38,8 @@
 				    <!--</ul>-->
 		       	</div><!--/.nav-collapse -->
           </div><!--.container -->
-        <!--</div><!--navbar-inverse -->
-	<!--</div>
+        <!--</div>--><!--navbar-inverse -->
+	<!--</div>-->
 	<!-- navbar-wrapper end" -->
 	
 <!-- Top nav bar end -->
